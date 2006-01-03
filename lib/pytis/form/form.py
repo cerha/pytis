7c364df6c0816707362e4be1a0812a8b518ae12f
cerha <cerha>
2006-01-03 15:05:26 +0000

    (LookupForm._lf_sfs_columns): Vracej úplně všechny definované sloupce.

--- lib/pytis/form/form.py
+++ lib/pytis/form/form.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-2 -*-
 
-# Copyright (C) 2001, 2002, 2003, 2004, 2005 Brailcom, o.p.s.
+# Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006 Brailcom, o.p.s.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -938,8 +938,7 @@ class LookupForm(RecordForm):
         return tuple(map(trans, self._lf_sorting))
 
     def _lf_sfs_columns(self):
-        columns = map(lambda id: self._view.field(id), self._view.columns())
-        return sfs_columns(columns, self._data,
+        return sfs_columns(self._view.fields(), self._data,
                            labelfunc=FieldSpec.column_label,
                            widthfunc=FieldSpec.column_width)
     
