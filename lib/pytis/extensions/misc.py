e6a3025976c78ef21aac1ae21043b48654fcae0c
cerha <cerha>
2006-01-04 15:47:23 +0000

    (get_menu_defs): Vynechávej položky týkající se konfigurace.

--- lib/pytis/extensions/misc.py
+++ lib/pytis/extensions/misc.py
@@ -654,8 +654,10 @@ def flatten_menus():
 def get_menu_defs(without_duals=False):
     resolver = pytis.form.resolver()
     RF = pytis.form.Application.COMMAND_RUN_FORM
+    config_items = config_menu_items()
     items = [item for item in flatten_menus()
-             if isinstance(item, pytis.form.MItem) and item.command() == RF]
+             if isinstance(item, pytis.form.MItem) and item.command() == RF \
+             and item not in config_items]
     duals = [item for item in items
              if issubclass(item.args()['form_class'], DualForm) \
              and not issubclass(item.args()['form_class'], DescriptiveDualForm)]
