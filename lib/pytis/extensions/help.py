d26ca10ac484d8438000eb8960fe2e66dcdfd391
cerha <cerha>
2006-02-14 11:25:32 +0000

    *** empty log message ***

---
+++ lib/pytis/extensions/help.py
@@ -0,0 +1,261 @@
+# -*- coding: iso-8859-2 -*-
+
+# Copyright (C) 2002, 2003, 2005, 2006 Brailcom, o.p.s.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""Tøídy a funkce pou¾ívané pøi generování nápovìdy k Pytis aplikaci.
+
+K vytváøení nápovìdy je vyu¾íváno LCG.  Ní¾e definované tøídy automaticky
+vytváøejí strukturu LCG dokumentù odpovídající specifikaci aktuální aplikace.
+Pøed pou¾itím je tøeba inicializovat konfiguraci Pytisu.
+
+Tøídy 'MenuIndex' a 'DescrIndex', mohou být pou¾ity v rámci nápovìdy aplikace
+formou Pythonových zdrojových souborù, které LCG automaticky detekuje a vyu¾ije
+k dynamickému vytvoøení vìtve v hierarchii dokumentù.  Více viz dokumentace
+LCG.
+
+""" 
+
+import sys, os
+import lcg, pytis.form, pytis.util
+
+
+global _used_defs
+_used_defs = []
+
+        
+def _fieldset(parent, pairs, title=None):
+    """Vra» instanci 'lcg.FieldSet' sestavenou ze seznamu dvojic øetìzcù."""
+    fields = [lcg.Field(parent, lcg.TextContent(parent, l),
+                        lcg.WikiText(parent, v)) for l,v in pairs]
+    return lcg.FieldSet(parent, fields, title=title)
+
+
+class ItemNode(lcg.ContentNode):
+    """Stránka s popisem koncové polo¾ky menu."""
+    
+    def __init__(self, parent, id, item, **kwargs):
+        self._item = item
+        super(ItemNode, self).__init__(parent, id, **kwargs)
+        
+    def _title(self):
+        return self._item.title()
+
+    def _create_content(self):
+        command, args = (self._item.command(), self._item.args())
+        hotkey = self._item.hotkey()
+        if hotkey and hotkey[0] is not None:
+            key = ' '.join(hotkey)
+        else:
+            key = _("Není definována")
+        if command.doc():
+            cmd = "%s (%s)" % (command.doc(), command.name())
+        else:
+            cmd = command.name()
+        info = [("Klávesová zkratka", key),
+                ("Pøíkaz", cmd)]
+        if command == pytis.form.Application.COMMAND_RUN_FORM:
+            name, form = (args['name'], args['form_class'])
+            info.append(("Typ formuláøe",
+                         '%s (%s)' % (form.DESCR.capitalize(), form.__name__)))
+            if not issubclass(form, pytis.form.ConfigForm):
+                if issubclass(form, pytis.form.BrowseDualForm):
+                    resolver = pytis.form.resolver()
+                    dual = resolver.get(name, 'dual_spec')
+                    node_name = name + '-dual'
+                    names = (node_name, dual.main_name(), dual.side_name())
+                else:
+                    node_name = name
+                    names = (name,)
+                global _used_defs
+                for n in names:
+                    if n not in _used_defs:
+                        _used_defs.append(n)
+                info.append(("Náhled", "[%s] (%s)" % (node_name, name)))
+        else:
+            a = ', '.join(['%s=%r' % x for x in args.items()])
+            info.append(("Argumenty pøíkazu", a or _("®ádné")))
+        return _fieldset(self, info)
+
+
+class MenuNode(ItemNode):
+    """Stránka s popisem polo¾ky menu, která obsahuje podmenu.
+
+    Polo¾ka obsahující podmenu sama o sobì nevyvolává ¾ádný pøíkaz, tak¾e
+    stránka obsahuje pouze seznam odkazù na jednotlivé polo¾ky podmenu.
+    
+    """
+    def _create_content(self):
+        return lcg.TableOfContents(self, depth=99)
+
+    def _create_children(self):
+        return [self._create_child(isinstance(item, pytis.form.MItem) and \
+                                   ItemNode or MenuNode,
+                                   '%s-%d' % (self._id, n+1), item)
+                for n, item in enumerate(self._item.items())
+                if isinstance(item, (pytis.form.MItem, pytis.form.Menu))]
+    
+        
+class MenuIndex(MenuNode):
+    """Koøenová stránka hierarchie menu aplikace.
+
+    Tato stránka nápovìdy vytvoøí hierarchii stránek popisujících jednotlivé
+    polo¾ky menu aktuální aplikace.
+
+    """
+    def __init__(self, parent, id, *args, **kwargs):
+        resolver = pytis.util.FileResolver('../defs')
+        pytis.form.NullApplication(resolver)
+        if '..' not in sys.path:
+            sys.path.append('..')
+        item = pytis.form.Menu(_("Pøehled menu"),
+                               resolver.get('application', 'menu'))
+        super(MenuIndex, self).__init__(parent, id, item, *args, **kwargs)
+
+    
+        
+################################################################################
+
+        
+class DescrNode(lcg.ContentNode):
+    """Stránka s popisem jednoduchého náhledu."""
+
+    
+    def __init__(self, parent, id, *args, **kwargs):
+        resolver = pytis.form.resolver()
+        self._read_spec(resolver, id)
+        super(DescrNode, self).__init__(parent, id, *args, **kwargs)
+
+
+    def _name(self, id):
+        return id
+
+    def _read_spec(self, resolver, id):
+        self._view_spec = resolver.get(id, 'view_spec')
+        self._data_spec = resolver.get(id, 'data_spec')
+        
+    def _title(self):
+        return self._view_spec.title()
+
+    def output_file(self):
+        return self.id().replace(':', '-') + '.html'
+
+    def _p(self, text):
+        return lcg.Paragraph(self, lcg.TextContent(self, text))
+        
+    def _create_content(self):
+        # Try to read the description from file.
+        content = [self._p(_("Název specifikace:") +' '+ self._name(self.id()))]
+        if os.path.exists(self._input_file(self._id, ext='txt')):
+            content.extend(self.parse_wiki_file(self._id, ext='txt'))
+        else:
+            # The file does not exist.  Let's read the specification.
+            content.append(self._p(self._default_description_text()))
+        content.extend(self._specific_info())
+        return content
+            
+    def _default_description_text(self):
+        return self._view_spec.description() or _("Popis není k dispozici.")
+
+    def _specific_info(self):
+        if not self._data_spec.access_rights():
+            return []
+        rights = self._data_spec.access_rights()
+        perms = [(perm, ', '.join([str(g) for g in
+                                   rights.permitted_groups(perm, None)]))
+                 for perm in (pytis.data.Permission.VIEW,
+                              pytis.data.Permission.INSERT,
+                              pytis.data.Permission.UPDATE,
+                              pytis.data.Permission.DELETE,
+                              pytis.data.Permission.EXPORT)]
+        return [lcg.Section(self, "Pøístupová práva",
+                            _fieldset(self, perms))]
+
+class DualDescrNode(DescrNode):
+    """Stránka s popisem duálního náhledu."""
+
+    def _name(self, id):
+        assert id.endswith('-dual')
+        return id[:-5]
+    
+    def _read_spec(self, resolver, id):
+        self._dual_spec = dual = resolver.get(self._name(id), 'dual_spec')
+        self._main_spec = resolver.get(dual.main_name(), 'view_spec')
+        self._side_spec = resolver.get(dual.side_name(), 'view_spec')
+
+    def _title(self):
+        return self._main_spec.title() +' / '+ self._side_spec.title()
+        
+    def _default_description_text(self):
+        def clabel(cid, view):
+            c = view.field(cid)
+            return c and c.column_label() or cid
+        dual, main, side = (self._dual_spec, self._main_spec, self._side_spec)
+        text = _("Hlavním formuláøem tohoto duálního formuláøe je '%s'.  "
+                 "V dolní èásti ze zobrazují související záznamy formuláøe "
+                 "'%s'.  Oba formuláøe jsou propojeny pøes shodu hodnot "
+                 "sloupeèkù '%s' = '%s'.") % \
+                 (main.title(), side.title(),
+                  clabel(dual.binding_column(), main),
+                  clabel(dual.side_binding_column(), side))
+        return text
+    
+    def _specific_info(self):
+        return [_fieldset(self, ((_("Horní formuláø"), \
+                                  "[%s]" % self._dual_spec.main_name()),
+                                 (_("Dolní formuláø"),
+                                  "[%s]" % self._dual_spec.side_name())))]
+    
+
+class DescrIndex(lcg.ContentNode):
+    """Koøenová stránka popisu pou¾itých náhledù aplikace.
+
+    Tato stránka nápovìdy vytvoøí jednotlivé podøízené stránky s popisem v¹ech
+    pou¾itých náhledù aktuální aplikace.  Seznam pou¾itých náhledù je vytváøen
+    pøi konstrukci nápovìdy hierarchie menu (tøída 'MenuIndex').  Vytvoøení
+    nápovìdy k menu je¹tì pøed vytváøením této stránky je tedy nezbytnou
+    podmínkou funkènosti této tøídy.
+
+    """
+
+    def _title(self):
+        return "Nápovìda k jednotlivým náhledùm"
+    
+    def _create_content(self):
+        return lcg.TableOfContents(self, depth=1)
+
+    def _create_children(self):
+        global _used_defs
+        _used_defs.sort()
+        cls = lambda name: name.endswith('-dual') and DualDescrNode or DescrNode
+        return [self._create_child(cls(name), name, subdir='descr')
+                for name in _used_defs]
+
+
+# def export(c):
+#     if isinstance(c, (lcg.TextContent, lcg.WikiText)):
+#         return c._text
+#     elif isinstance(c, lcg.Paragraph):
+#         return ' '.join([export(x) for x in c._content])
+#     elif isinstance(c, lcg.ItemizedList):
+#         return "\n\n".join([" * "+export(x) for x in c._content])
+#     elif isinstance(c, lcg.Section):
+#         return "\n\n".join([export(x) for x in c._content
+#                             if not isinstance(x, lcg.Section)])
+#     elif isinstance(c, lcg.Container):
+#         return "\n\n".join([export(x) for x in c.content()])
+#     else:
+#         print "***", c
