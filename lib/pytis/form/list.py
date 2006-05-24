8511178e24f844cdc3070d4f426fa94e2408966a
cerha <cerha>
2006-05-24 14:57:18 +0000

    (BrowseForm._action_mitems): Titulky akcí získávej s `raw=True'.

--- lib/pytis/form/list.py
+++ lib/pytis/form/list.py
@@ -2009,10 +2009,11 @@ class BrowseForm(ListForm):
         for x in spec:
             if isinstance(x, Action):
                 cmd = ListForm.COMMAND_CONTEXT_ACTION(action=x)
-                items.append(MItem(x.title(), command=cmd,
+                items.append(MItem(x.title(raw=True), command=cmd,
                                    help=x.descr()))
             elif isinstance(x, ActionGroup):
-                items.append(Menu(x.title(), self._action_mitems(x.actions())))
+                items.append(Menu(x.title(raw=True),
+                                  self._action_mitems(x.actions())))
             elif isinstance(x, (types.TupleType, types.ListType)):
                 if items:
                     items.append(MSeparator())
