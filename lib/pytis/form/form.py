8112e9459bae2046bdc3178c7dd97b3686b6df1b
cerha <cerha>
2006-11-02 15:49:22 +0000

    (LookupForm._init_sorting): Kontroluj platnost specifikace třídění i pokud bylo předáno jako argument konstruktoru.

--- lib/pytis/form/form.py
+++ lib/pytis/form/form.py
@@ -1,7 +1,7 @@
 # -*- coding: iso-8859-2 -*-
 
 # Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006 Brailcom, o.p.s.
-s#
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
