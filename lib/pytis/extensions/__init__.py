45ac02db676c1b437b939d0b28e7b49918c3d9f0
cerha <cerha>
2007-01-24 07:19:41 +0000

    Přidán hack s nastavením výchozího kódování před importem `pytis.form'.  Odstraňen hack s podmíněným importem některých modulů.

--- lib/pytis/extensions/__init__.py
+++ lib/pytis/extensions/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-2 -*-
 #
-# Copyright (C) 2005, 2006 Brailcom, o.p.s.
+# Copyright (C) 2005, 2006, 2007 Brailcom, o.p.s.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,31 +16,27 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import pytis.data
+import sys
+if sys.getdefaultencoding() != 'iso-8859-2':
+    reload(sys)
+    sys.setdefaultencoding('iso-8859-2')
+
+import pytis.data, pytis.form
 
 from dbconfig import *
 from dbutils import *
 from misc import *
 from types import *
 from spec import *
+from defs import *
 
-for file in (dbconfig, dbutils, misc, types, spec):
+for file in (dbconfig, dbutils, misc, types, spec, defs):
     file.__dict__.update(globals())
 
-if hasattr(pytis, 'form'):
-    # Starší defsy počítají s tím, že importem pytis.extensions jsou
-    # importovány i všechny identifikátory z pytis.form.  Protože však
-    # nechceme, aby pytis.extensions závisely na pytis.form a tím potažmo na
-    # wx, importujeme tyto identifikátory jen pokud jsou pytis.extensions
-    # používány z wx aplikace, v kterémžto případě by modul pytis.form již měl
-    # být natažen.  Časem by bylo dobré to nedělat vůbec - defsy by na
-    # formulářích neměly záviset.  Pokud závisí (interaktivní obslužné rutiny
-    # používající formuláře či dialogy), nechť si potřebné identifikátory
-    # importují vlastnoručně.
-    from pytis.form import *
-    # Modul defs je také závislý na pytis.form, takže jej chceme importovat
-    # jen v případě běhu wx aplikace.
-    from defs import *
-    defs.__dict__.update(globals())
+# Většina defsů počítají s tím, že importem pytis.extensions jsou importovány i
+# všechny identifikátory z pytis.form.  Časem by bylo dobré se toho zbavit a
+# používat celé názvy `pytis.forms.*'.  Zde je import každopádně až na konci,
+# abychom "nezasvinili" jmenný prostor modulů.
+from pytis.form import *
 
     
