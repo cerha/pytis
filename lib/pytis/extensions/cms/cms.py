614621788818617371d3453072493fbde47616af
cerha <cerha>
2009-02-27 13:17:30 +0000

    Added generic Pytis CMS.

---
+++ lib/pytis/extensions/cms/cms.py
@@ -0,0 +1,150 @@
+# -*- coding: iso-8859-2 -*-
+#
+# Copyright (C) 2009 Brailcom, o.p.s.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Pytis CMS specifications.
+
+Pytis CMS is a fat client application for management of database structures, which define the
+behavior of a web application.
+
+Wiking modules which implement a Wiking application, which reads these database structures and
+behaves according to them can be found in the 'web' submodule of this module.
+
+"""
+
+import lcg
+import pytis.data as pd, pytis.presentation as pp
+from pytis.presentation import Specification, FieldSpec as Field, Fields, \
+     HGroup, VGroup, Binding, CodebookSpec, Computer, CbComputer, computer
+from pytis.extensions import nextval, ONCE, NEVER, ALWAYS
+ASC = pd.ASCENDENT
+DESC = pd.DESCENDANT
+
+
+class Languages(Specification):
+    title = _("Jazyky")
+    help = _("Správa jazykù dostupných v CMS.")
+    table = 'cms_languages'
+    def fields(self): return (
+        Field('lang_id', default=nextval('cms_languages_lang_id_seq')),
+        Field('lang', _("Kód"), width=2, column_width=6, fixed=True,
+              filter=pp.TextFilter.ALPHANUMERIC, post_process=pp.PostProcess.LOWER),
+        Field('name', _("Název"), virtual=True,
+              computer=computer(lambda record, lang: self._language_name(lang))),
+        )
+    def _language_name(self, lang):
+        t = lcg.GettextTranslator('cs', path=(lcg.config.translation_dir,), fallback=True)
+        return t.translate(lcg.language_name(lang))
+    def cb(self):
+        return CodebookSpec(display=lambda lang: self._language_name(lang), prefer_display=True)
+    sorting = ('lang', ASC),
+    layout = ('lang',)
+    columns = ('lang', 'name')
+
+class Modules(Specification):
+    title = _("Moduly")
+    help = _("Správa roy¹iøujících modulù pou¾itelných ve stránkách.")
+    table = 'cms_modules'
+    def fields(self): return (
+        Field('mod_id', default=nextval('cms_modules_mod_id_seq')),
+        Field('modname', _("Název"), width=32),
+        Field('descr', _("Popis"), width=64, virtual=True,
+              computer=computer(self._modtitle))
+        )
+    def _modtitle(self, row, modname):
+        return None
+    sorting = ('modname', ASC),
+    cb = CodebookSpec(display='modname', prefer_display=True)
+    layout = ('modname', 'descr')
+    columns = ('modname', 'descr')
+    
+
+class MenuParents(Specification):
+    # Codebook of parent items for Menu (to prevent recursion).
+    title = _("Menu")
+    table = 'cms_menu'
+    def fields(self): return (
+        Field('menu_id'),
+        Field('menu_item_id'),
+        Field('tree_order', type=pd.TreeOrder()),
+        Field('lang'),
+        Field('title_or_identifier', _("Název"), width=32),
+        Field('identifier', _("Identifikátor"), width=20),
+        Field('modname', _("Modul")),
+        #Field('description', _("Popis"), width=64),
+        )
+    sorting = ('tree_order', ASC),
+    columns = ('title_or_identifier', 'identifier', 'modname')
+    cb = CodebookSpec(display='title_or_identifier', prefer_display=True)
+    
+
+class Menu(Specification):
+    title = _("Menu")
+    help = _("Správa polo¾ek hlavního menu, jejich hierarchie a obsahu.")
+    table = 'cms_menu'
+    def _cbname(self, name):
+        # Hack to allow namespaced spec names in wx app and plain module names in Wiking (the
+        # specification is inherited by the Wiking Module).
+        return 'cms.'+ name
+    def _parent_filter(self, record, lang):
+        return pd.EQ('lang', pd.Value(pd.String(), lang))
+    def fields(self): return (
+        Field('menu_id'),
+        Field('menu_item_id'),
+        Field('tree_order', type=pd.TreeOrder()),
+        Field('identifier', _("Identifikátor"), width=20, fixed=True, editable=ONCE,
+              type=pd.RegexString(maxlen=32, not_null=True, regex='^[a-zA-Z][0-9a-zA-Z_-]*$'),
+              descr=_("Identifikátor bude vystupovat jako vnìj¹í adresa stránky.  Mù¾e být pou¾it "
+                      "v odkazech na tuto stránku v rámci textu jiných stránek. Platný "
+                      "identifikátor mù¾e obsahovat pouze písmena bez diakritiky, èíslice, "
+                      "pomlèky a podtr¾ítka a musí zaèínat písmenem.")),
+        Field('lang', _("Jazyk"), editable=ONCE, codebook=self._cbname('Languages'),
+              value_column='lang', selection_type=pp.SelectionType.CHOICE),
+        Field('title_or_identifier', _("Název"), width=30),
+        Field('title', _("Název"), width=32, not_null=True),
+        Field('description', _("Popis"), width=64,
+              descr=_("Struèný popis stránky (zobrazen v menu jako tooltip).")),
+        Field('content', _("Obsah"), type=pd.StructuredText(), compact=True, height=20, width=80,
+              descr=_("Text stránky formátovaný jako LCG strukturovaný text (wiki)")),
+        Field('mod_id', _("Modul"), not_null=False,
+              codebook=self._cbname('Modules'),
+              descr=_("Vyberte roz¹iøující modul zobrazený uvnitø stránky.  Ponechte prázdné pro "
+                      "prostou textovou stránku.")),
+        Field('modname', _("Modul")),
+        Field('parent', _("Nadøízená polo¾ka"), not_null=False,
+              codebook=self._cbname('MenuParents'), value_column='menu_item_id',
+              runtime_filter=computer(self._parent_filter),
+              descr=_("Vyberte bezprostøednì nadøízenou polo¾ku v hierarchii menu.  Ponechte "
+                      "prázdné pro stránky na nejvy¹¹í úrovni menu.")),
+        Field('published', _("Zveøejnìno"), width=6, default=True, fixed=True, 
+              descr=_("Umo¾òuje øídit dostupnost dané polo¾ky nezávisle pro ka¾ou jazykovou "
+                      "verzi.")),
+        Field('ord', _("Poøadí"), width=8, editable=ALWAYS, fixed=True, 
+              descr=_("Zadejte èíslo urèující poøadí polo¾ky v menu (mezi stránkami na stejné "
+                      "úrovni hierarchie.  Pokud nevyplníte, stránka bude automaticky zaøazena "
+                      "na konec.")),
+        )
+    def row_style(self, record):
+        if not record['published'].value():
+            return pp.Style(foreground='#777', background='#eee')
+        else:
+            return None
+    sorting = ('tree_order', ASC),
+    layout = (VGroup('identifier', 'parent', 'mod_id', 'ord',
+                     label=_("Globální vlastnosti (spoleèné pro v¹echny jazyky)")),
+              VGroup('lang', 'title', 'description', 'content', 'published',
+                     label=_("Text stránky (pro aktuální jazykovou verzi)")))
+    columns = ('title_or_identifier', 'identifier', 'modname', 'ord', 'published')
---
+++ lib/pytis/extensions/cms/web.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006, 2007, 2008, 2009 Brailcom, o.p.s.
+# Author: Tomas Cerha.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""Wiking modules for web applications based on Pytis CMS.
+
+Pytis CMS is a fat client application for management of database structures, which define the
+behavior of a web application.  Wiking modules defined here implement a Wiking application, which
+reads these database structures and behaves according to them.
+
+Example application using these Wiking modules can be found in pytis-demo.
+
+"""
+
+import os, re, lcg, wiking, pytis.util
+import cms
+
+
+class Languages(wiking.PytisModule):
+    class Spec(wiking.Specification, cms.Languages):
+        pass
+
+    def languages(self):
+        return [str(r['lang'].value()) for r in self._data.get_rows()]
+
+
+class Menu(wiking.PytisModule):
+
+    class Spec(wiking.Specification, cms.Menu):
+        def _cbname(self, name):
+            # Some codebooks are not needed for the web module, so just ignore them.
+            if name in ('Languages'):
+                return name
+            else:
+                return None
+    
+    _SEPARATOR = re.compile('^====+\s*$', re.MULTILINE)
+
+    @classmethod
+    def binding_id(cls, modname):
+        return pytis.util.camel_case_to_lower(modname, '-')
+    
+    def _resolve(self, req):
+        identifier = req.unresolved_path[0]
+        rows = self._data.get_rows(identifier=identifier, published=True)
+        if not rows:
+            if self._data.get_rows(identifier=identifier):
+                raise wiking.Forbidden()
+            else:
+                raise wiking.NotFound()
+        variants = [str(row['lang'].value()) for row in rows]
+        lang = req.prefered_language(variants)
+        del req.unresolved_path[0]
+        return rows[variants.index(lang)]
+
+    def _bindings(self, req, record):
+        bindings = super(Menu, self)._bindings(req, record)
+        modname = record['modname'].value()
+        if modname:
+            cls = wiking.cfg.resolver.wiking_module_cls(modname)
+            if cls and issubclass(cls, EmbeddablePytisModule):
+                bindings.append(cls.binding())
+        return bindings
+    
+    def menu(self, req):
+        children = {None: []}
+        translations = {}
+        def item(row):
+            menu_item_id = row['menu_item_id'].value()
+            identifier = str(row['identifier'].value())
+            titles, descriptions = translations[menu_item_id]
+            title = lcg.SelfTranslatableText(identifier, translations=titles)
+            descr = lcg.SelfTranslatableText('', translations=descriptions)
+            modname = row['modname'].value()
+            if modname is not None:
+                try:
+                    module = self._module(modname)
+                except AttributeError:
+                    # We want the website to work even if the module was uninstalled or renamed. 
+                    submenu = []
+                else:
+                    submenu = list(module.submenu(req, menu_item_id))
+            else:
+                submenu = []
+            submenu += [item(r) for r in children.get(menu_item_id, ())]
+            hidden = False # TODO: get visibility for the current user
+            return wiking.MenuItem(identifier, title, descr=descr, hidden=hidden,
+                                   variants=titles.keys(), submenu=submenu)
+        # First process all rows and build a dictionary of descendants for each item and
+        # translations of titles and descriptions.  Then construct the menu structure.
+        for row in self._data.get_rows(sorting=self._sorting, published=True):
+            menu_item_id = row['menu_item_id'].value()
+            if not translations.has_key(menu_item_id):
+                parent = row['parent'].value()
+                if not children.has_key(parent):
+                    children[parent] = []
+                children[parent].append(row)
+                translations[menu_item_id] = ({}, {})
+            titles, descriptions = translations[menu_item_id]
+            lang = str(row['lang'].value())
+            titles[lang] = row['title_or_identifier'].value()
+            if row['description'].value() is not None:
+                descriptions[lang] = row['description'].value()
+        return [item(row) for row in children[None]]
+               # TODO: Add hidden menu items for static mapping items.
+               #[MenuItem('_doc', _("Wiking Documentation"), hidden=True)]
+    
+    def action_view(self, req, record, err=None, msg=None):
+        # Main content
+        modname = record['modname'].value()
+        if modname is not None:
+            module = self._module(modname)
+            content = module.embed(req, record)
+            if isinstance(content, int):
+                # The request has already been served by the embedded module. 
+                return content
+        else:
+            content = []
+        text = record['content'].value()
+        if text:
+            if self._SEPARATOR.search(text):
+                pre, post = self._SEPARATOR.split(text, maxsplit=2)
+            else:
+                pre, post = text, ''
+            parser = lcg.Parser()
+            sections = parser.parse(pre) + content + parser.parse(post)
+            content = [lcg.SectionContainer(sections, toc_depth=0)]
+        if not content and record['parent'].value() is None:
+            rows = self._data.get_rows(parent=record['menu_item_id'].value(), published=True,
+                                       sorting=self._sorting)
+            # TODO: Use only items, which are visible to the current user (access rights). 
+            if rows:
+                return req.redirect('/'+rows[0]['identifier'].value())
+        return self._document(req, content, record, err=err, msg=msg)
+    
+    # TODO: Handle embeded modules without binding (not EmbeddablePytisModule).
+    #def action_subpath(self, req, record):
+    #    return super(Menu, self).action_subpath(req, record)
+    
+    def module_uri(self, modname):
+        row = self._data.get_row(modname=modname)
+        if row:
+            return '/'+ row['identifier'].value() +'/'+ self.binding_id(row['modname'].value())
+        else:
+            return None
+    
+    
+class Application(wiking.CookieAuthentication, wiking.Application):
+
+    _MAPPING = dict(wiking.Application._MAPPING,
+                    _resources='Resources')
+    
+    def menu(self, req):
+        return self._module('Menu').menu(req)
+
+    def languages(self):
+        return self._module('Languages').languages()
+
+    def handle(self, req):
+        if req.unresolved_path:
+            try:
+                modname = self._MAPPING[req.unresolved_path[0]]
+            except KeyError:
+                modname = 'Menu'
+            else:
+                # Consume the unresolved path if it was in static mapping or leave it for further
+                # resolution when passing to Menu.
+                del req.unresolved_path[0]
+            return req.forward(self._module(modname))
+        else:
+            return super(Application, self).handle(req)
+    
+    #def panels(self, req, lang):
+    #    return
+    
+    #def _auth_user(self, req, login):
+    #    return self._module('Authentication').user(req, login)
+        
+    #def _auth_check_password(self, user, password):
+    #    return user.data().check_passwd(password)
+    
+    #def authorize(self, req, module, action=None, record=None, **kwargs):
+    #    return True
+
+    
+class Embeddable(object):
+    """Mix-in class for modules which may be embedded into page content.
+
+    Derived classes must implement the 'embed()' method to produce the content to be embedded into
+    page text.  The method 'submenu()' also allows the module to extend the main menu.
+
+    """
+    
+    def submenu(self, req, menu_item_id):
+        """Return a list of 'MenuItem' instances to insert into the main menu.
+        
+        The submenu will appear in the main menu under the item of a page which embeds the module.
+        The items returned by this method will always be placed above any items defined by the menu
+        structure.
+
+        """
+        return []
+    
+    def embed(self, req, record):
+        """Return a list of content instances extending the page content.
+
+        The returned value can also be an integer to indicate that the request has already been
+        served (with the resulting status code).
+        
+        """
+        return []
+
+
+
+class EmbeddablePytisModule(wiking.PytisModule, Embeddable):
+    _EMBED_BINDING_COLUMN = None
+
+    @staticmethod
+    def _embed_condition(row):
+        return None
+    
+    @classmethod
+    def binding(cls):
+        return wiking.Binding(cls.title(), cls.name(), cls._EMBED_BINDING_COLUMN,
+                              condition=cls._embed_condition,
+                              id=Menu.binding_id(cls.name()))
+    
+    def embed(self, req, record):
+        return [self.related(req, self.binding(), record, req.uri())]
+
