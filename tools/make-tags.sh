6773b7b00daa25b350d2433b37feff4e720f6095
Tomas Cerha <cerha@brailcom.org>
2009-05-07 17:24:54 +0200

    Create TAGS table in the package root directory, bot in `lib'.

--- tools/make-tags.sh
+++ tools/make-tags.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DIRNAME=$(dirname $0)
-SRCDIR=$DIRNAME/../lib
-if [ ! -d $SRCDIR/pytis ]; then
+BASEDIR=$DIRNAME/..
+if [ ! -d $BASEDIR/lib/pytis ]; then
   echo 'Source directory not found'
   exit -1
 fi
 
-cd $SRCDIR
+cd $BASEDIR
 rm -f TAGS
-find -name '*.py' -not -name '_test.py' | xargs etags --append --regex='/^[ \t]+def[ \t]+\([a-zA-Z_0-9]+\)/\1/'
+find lib/pytis -name '*.py' -not -name '_test.py' | xargs etags --append --regex='/^[ \t]+def[ \t]+\([a-zA-Z_0-9]+\)/\1/'
