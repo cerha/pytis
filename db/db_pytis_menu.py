9c0dec47371e415bbbfe62f89e51bf5c4efa0598
Milan Zamazal <pdm@brailcom.org>
2009-06-03 18:46:47 +0200

    New gensql schema for dynmenu started
    Conflicts:
    
    	db/db_pytis_menu.py

--- db/db_pytis_menu.py
+++ db/db_pytis_menu.py
@@ -1,95 +1,215 @@
-#-*- coding: utf-8 -*-
-"""Tento soubor obsahuje definice databázových objektů pro definici menu aplikace
-udržované v přímo v databázi. Do hlavního db.py jednotlivých projektů
-se přidá symbolickým přilinkováním do odpovídajícího adresáře db v projektu
-a přidáním příkazů
+# -*- coding: utf-8 -*-
 
-Gpytis_menu = <práva pro objekty tohoto souboru>
-execfile('db_pytis_menu.py', copy.copy(globals()))
-"""
+"""Gensql definitions for dynamic application menus and access rights."""
 
-db_rights = globals().get('Gpytis_menu', None)
+import pytis.data
 
+C = Column
+P = PrimaryColumn
+V = ViewColumn
+
+TBoolean = pytis.data.Boolean()
+TDate = pytis.data.Date()
+TDateTime = pytis.data.DateTime()
+TInteger = pytis.data.Integer()
+TSerial = pytis.data.Serial()
+TString = pytis.data.String()
+TUser = 'name'
+
+#db_rights = globals().get('Gpytis_menu', None)
+db_rights = (('all', 'pdm',),)
 if not db_rights:
     raise ProgramError('No rights specified! Please define Gpytis_menu')
 
+def Ctimestamp(name, doc=None):
+    return C(name, TDateTime, constraints=('NOT NULL',), default='now()', doc=doc)
+def Cuser(name, doc=None):
+    return C(name, TUser, constraints=('NOT NULL',), default='user', doc=doc)
 
-table('_pytis_menu_actions',
-      (P('id', varchar(3)),
-       C('action', TString),
-       ),
+table('_changes',
+      (Cuser('vytvoril'),
+       Ctimestamp('vytvoreno'),
+       Cuser('zmenil'),
+       Ctimestamp('zmeneno')),
       grant=db_rights,
-      doc="Codebook of pytis menu actions.",
-      init_columns=('id', 'action'),
-      init_values=(
-        ("'bf'", "'BrowseForm'"),
-        ("'df'", "'BrowseDualForm'"),
-        ("'sf'", "'ShowForm'"),
-        ("'ddf'", "'DescriptiveDualForm'"),
-        ("'ef'", "'PopupEditForm'"),
-        ("'nr'", "'New Record'"),
-        ("'rp'", "'Run Procedure'"),
-      )
-      
-table('_pytis_spec',
-      (P('id', TSerial),
-       C('main_spec', TString, constraints=('NOT NULL',)),
-       C('side_spec', TString),
-       C('shortcut', TString),
-       C('action', varchar(3), references='_pytis_menu_actions ON UPDATE CASCADE ON DELETE CASCADE'),
-       ),
-      grant=db_rights,
-      doc="""Stores combinations of specifications used by _pytis_menu."""      
+      doc="""Sloupečky zaznamenávající uživatele a časy vytvoření a změn údajů.
+      Je určena k tomu, aby ji dědily všechny ostatní tabulky."""
       )
 
-table('_pytis_menu',
-      (P('id', TSerial),
-       C('module', TString),
-       C('title', TString, constraints=('NOT NULL',)),
-       C('id_spec', TInteger, references='_pytis_actions ON UPDATE CASCADE ON DELETE CASCADE',
-         constraints=('NOT NULL',)),
-       C('id_parent', TInteger, references='_pytis_menu'),
-       C('ordering', TString)
+table('_inserts',
+      (P('id', TSerial,
+         doc="identifikace řádku"),
+       Cuser('vytvoril'),
+       Ctimestamp('vytvoreno'),
+       C('tabulka', TString),
+       C('klic', TString),
        ),
+      view=(TableView((V(None, 'datum', 'vytvoreno::date'),
+                       V(None, 'cas', 'vytvoreno::time'),
+                       V('oid', 'oid')),
+                      exclude=(),
+                      name='v_inserts',
+                      grant=db_rights,
+                      update=None,
+                      insert=None,
+                      delete=None),
+            TableView((V(None, 'datum', 'vytvoreno::date'),
+                       V(None, 'cas', 'vytvoreno::time'),
+                       V('oid', 'oid')),
+                      exclude=(),
+                      join="vytvoril = current_user",
+                      name='v_inserts_user',
+                      grant=db_rights,
+                      update=None,
+                      insert=None,
+                      delete=None)),
       grant=db_rights,
-      doc="""Stores menu structure."""      
+      doc="""Tabulka zaznamenávající přidávání záznamů standardních
+      tabulek."""
       )
 
+table('_updates',
+      (P('id', TSerial,
+         doc="identifikace změnového řádku"),
+       Cuser('zmenil'),
+       Ctimestamp('zmeneno'),
+       C('tabulka', TString),
+       C('klic', TString),
+       C('zmeny', TString)
+       ),
+      view=(TableView((V(None, 'datum', 'zmeneno::date'),
+                       V(None, 'cas', 'zmeneno::time'),
+                       V('oid', 'oid')),
+                      exclude=(),
+                      name='v_updates',
+                      grant=db_rights,
+                      update=None,
+                      insert=None,
+                      delete=None),
+            TableView((V(None, 'datum', 'zmeneno::date'),
+                       V(None, 'cas', 'zmeneno::time'),
+                       V('oid', 'oid')),
+                      exclude=(),
+                      join="zmenil = current_user",
+                      name='v_updates_user',
+                      grant=db_rights,
+                      update=None,
+                      insert=None,
+                      delete=None)),
+      grant=db_rights,
+      doc="""Tabulka zaznamenávající změny v záznamech standardních
+      tabulek."""
+      )
 
-      CREATE OR REPLACE FUNCTION _tree_order(menu_id int) RETURNS text AS $$
-  SELECT
-    CASE WHEN $1 IS NULL THEN '' ELSE
-      (SELECT _tree_order(parent) || '.' ||
-       to_char(coalesce(ord, 999999), 'FM000000')
-       FROM menu where menu_id=$1)
-    END
-  AS RESULT
-$$ LANGUAGE SQL;
-      
-function('_pytis_menu_ordering',
-         (TInteger,),
-         TString,
-         body=("insert into _pytis_config (uzivatel, config) "
-               "select current_user, NULL::text "
-               "where (select current_user) not in "
-               "(select uzivatel from _pytis_config); "
-               "select config from _pytis_config "
-               "where uzivatel = (select current_user)"),
-         doc="""Funkce na zjišťování configu pro daného uživatele""",
-         depends=('_pytis_config',))
+table('_deletes',
+      (P('id', TSerial,
+         doc="identifikace řádku"),
+       Cuser('smazal'),
+       Ctimestamp('smazano'),
+       C('tabulka', TString),
+       C('klic', TString)),
+      view=(TableView((V(None, 'datum', 'smazano::date'),
+                      V(None, 'cas', 'smazano::time'),
+                      V('oid', 'oid')),
+                     exclude=(),
+                     name='v_deletes',
+                     grant=db_rights,
+                     update=None,
+                     insert=None,
+                     delete=None),
+            TableView((V(None, 'datum', 'smazano::date'),
+                       V(None, 'cas', 'smazano::time'),
+                       V('oid', 'oid')),
+                      exclude=(),
+                      join = "smazal = current_user",
+                      name='v_deletes_user',
+                      grant=db_rights,
+                      update=None,
+                      insert=None,
+                      delete=None)),            
+      grant=db_rights,
+      doc="""Tabulka zaznamenávající vymazávání záznamů ve standardních
+      tabulkách."""
+      )
+def _log_update_trigger():
+    def pg_escape(val):
+        return val.replace("'", "''")
+    event = TD["event"]
+    if event == "DELETE":
+        newold = "old"
+        table = "_deletes"
+    elif event == "INSERT":
+        newold = "new"
+        table = "_inserts"
+    else:
+        newold = "new"
+        table = "_updates"
+    tabid = TD["relid"]
+    q = "select relname from pg_class where oid = %s" % tabid
+    q = plpy.execute(q)
+    tabulka = q[0]["relname"]
+    klice = TD["args"][0].split(',')
+    klicestr = ','.join(["%s: %s" % (k, str(TD[newold][k]))
+                         for k in klice])
+    # pro INSERT a DELETE zaznamenáme tabulku a klíč
+    if event in ("DELETE", "INSERT"):
+        q = """insert into %s (tabulka, klic)
+               select '%s', '%s'
+            """ % (table, tabulka, klicestr)
+        q = plpy.execute(q)
+        return None
+    # Pro UPDATE zaznamenáme kromě jména tabulky a klíče i změny v položkách
+    zmeny = []
+    for k in TD["new"].keys():
+        if TD["new"][k] != TD["old"][k]:
+            zmeny.append("""%s: %s -> %s""" % (k, pg_escape(str(TD["old"][k])),
+                                               pg_escape(str(TD["new"][k]))))
+    if zmeny != []:        
+        zmenystr = """\n""".join(zmeny)        
+        q = """insert into _updates (tabulka, klic, zmeny)
+               select '%s', '%s', '%s'
+            """ % (tabulka, klicestr, zmenystr)
+        q = plpy.execute(q)
+    return None
 
-function('write_pytis_config',
-         (TString,),
-         TString,
-         body=("update _pytis_config set config = $1 "
-               "where uzivatel = (select current_user);"
-               "insert into _pytis_config (uzivatel, config) "
-               "select current_user, $1::text "
-               "where (select current_user) not in "
-               "(select uzivatel from _pytis_config); "               
-               "select config from _pytis_config "
-               "where uzivatel = (select current_user)"
-               ),
-         doc="""Funkce na zápis configu pro daného uživatele.""",
-         depends=('_pytis_config',))
+function('_log_update_trigger', (), 'trigger',
+         body=_log_update_trigger,
+         doc="""Slouží k evidenci editací nad záznamy tabulek.""",
+         depends=('_inserts', '_deletes', '_updates'))
 
+def _std_table(name, columns, doc, grant=db_rights, **kwargs):
+    return table(name, columns, inherits=('_changes',), grant=grant,
+                 doc=doc, upd_log_trigger='_log_update_trigger',
+                 **kwargs)
+
+
+### The menu schema itself
+
+_std_table('e_pytis_roles',
+           (P('roleid', TSerial),
+            C('name', TUser, constraints=('unique', 'not null',),),
+            C('description', 'varchar(32)'),
+            C('system', TBoolean, constraints=('not null',), default="'F'",
+              doc="Identifies roles corresponding to system accounts"),
+            C('admin', TBoolean, constraints=('not null',), default="'F'",
+              doc="Identifies roles of menu and access administrators"),
+            C('deleted', TDate)),
+            """Application user roles.
+There are three kinds of roles:
+1. Roles corresponding to system accounts (login roles).
+2. Menu and access administrator roles.  Rows of these roles may be changed
+   only by the database administrators.
+3. Pure application roles.
+"""
+            )
+
+_std_table('e_pytis_role_members',
+           (P('id', TSerial,
+              doc="Just to make logging happy"),
+            C('roleid', TInteger, constraints=('not null',), references='e_pytis_roles'),
+            C('member', TInteger, constraints=('not null',), references='e_pytis_roles'),
+            ),
+           """Mutual memberships of roles.
+Entries in this table define `member's of each `roleid'.
+""",
+           depends=('e_pytis_roles',))
