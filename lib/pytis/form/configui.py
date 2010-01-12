8317deff99a3fd2b680b58e9ab69f8d5294fdd06
Tomas Cerha <cerha@brailcom.org>
2010-01-12 20:34:20 +0100

    Only save options present in the updated row in the config form.

--- lib/pytis/form/configui.py
+++ lib/pytis/form/configui.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-2 -*-
 
-# Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 Brailcom, o.p.s.
+# Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010 Brailcom, o.p.s.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -138,8 +138,7 @@ class _ConfigData(pytis.data.RestrictedData):
         return pytis.data.Row(row_data)
 
     def update(self, key, row, transaction=None):
-        options = [c.id() for c in self.columns()]
-        for option in options:
+        for option in row.keys():
             setattr(config, option, row[option].value())
         wx.ToolTip.Enable(config.show_tooltips)
         return row, True
