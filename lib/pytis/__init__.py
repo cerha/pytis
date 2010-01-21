c79de68598d7359bc04d4de5826a96ca9780209d
Tomas Cerha <cerha@brailcom.org>
2010-01-21 19:33:27 +0100

    Released version 1.0.0.

--- lib/pytis/__init__.py
+++ lib/pytis/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-2 -*-
 
-# Copyright (C) 2001, 2002, 2006 Brailcom, o.p.s.
+# Copyright (C) 2001, 2002, 2006, 2010 Brailcom, o.p.s.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 import util
 import data
