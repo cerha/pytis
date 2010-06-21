7d794ab9cbf1366baa9b8c1a856790161d493bbb
Pavel Hanak <hanak@wpb.cz>
2010-06-21 17:23:50 +0200

    Take into account db schemas for statistics definitions.

--- db/db_statistics.py
+++ db/db_statistics.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+db_schemas = globals().get('Gpytis_config_schemas', None)
+
 # Form statistics
 
 _std_table_nolog('e_pytis_form_log',
@@ -11,6 +13,7 @@ _std_table_nolog('e_pytis_form_log',
                   C('t_start', TDateTime, constraints=('NOT NULL',)),
                   C('t_show', TDateTime, constraints=('NOT NULL',)),
                   ),
+                 schemas=db_schemas,
                  doc="""
 Statistics about using forms by users and form opening performance.
 form is fully qualified specification name.
@@ -20,6 +23,7 @@ info is optional extra information provided by the form (e.g. sorting used).
 t_start is the time when user invoked the form opening command.
 t_show is the time when the form got actually ready for operation after its start.
 """)
+
 sql_raw("""
 create index e_pytis_form_log_form_index on e_pytis_form_log(form);
 create index e_pytis_form_log_info_index on e_pytis_form_log(info);
@@ -27,6 +31,7 @@ create index e_pytis_form_log_class_index on e_pytis_form_log(class);
 create index e_pytis_form_log_user_index on e_pytis_form_log(login);
 create index e_pytis_form_log_tstart_index on e_pytis_form_log(t_start);
 """,
+        schemas=db_schemas,
         depends=('e_pytis_form_log',))
 
 function('pytis_log_form', (TString, TString, TString, TDateTime, TDateTime), TInteger,
@@ -34,6 +39,7 @@ function('pytis_log_form', (TString, TString, TString, TDateTime, TDateTime), TI
 insert into e_pytis_form_log (form, class, info, login, t_start, t_show) values($1, $2, $3, user, $4, $5) returning id;
 """,
          grant=Gall_pytis,
+         schemas=db_schemas,
          depends=('e_pytis_form_log',))
 
 viewng('ev_pytis_form_summary',
@@ -49,6 +55,7 @@ viewng('ev_pytis_form_summary',
        update=None,
        delete=None,
        grant=Gall_pytis,
+       schemas=db_schemas,
        depends=('e_pytis_form_log',)
        )
 
@@ -65,6 +72,7 @@ viewng('ev_pytis_form_short_summary',
        update=None,
        delete=None,
        grant=Gall_pytis,
+       schemas=db_schemas,
        depends=('e_pytis_form_log',)
        )
 
@@ -79,6 +87,7 @@ viewng('ev_pytis_form_users',
        update=None,
        delete=None,
        grant=Gall_pytis,
+       schemas=db_schemas,
        depends=('e_pytis_form_log',)
        )
 
@@ -95,6 +104,7 @@ viewng('ev_pytis_form_users_noinfo',
        update=None,
        delete=None,
        grant=Gall_pytis,
+       schemas=db_schemas,
        depends=('e_pytis_form_log',)
        )
 
@@ -106,5 +116,6 @@ viewng('ev_pytis_form_user_list',
        update=None,
        delete=None,
        grant=Gall_pytis,
+       schemas=db_schemas,
        depends=('e_pytis_form_log',)
        )
