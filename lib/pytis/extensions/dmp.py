ff9bbfaef864353dcab92a264b492323acf25387
Milan Zamazal <pdm@brailcom.org>
2010-08-25 17:32:13 +0200

    Fix of alternate fullname construction in dmp

--- lib/pytis/extensions/dmp.py
+++ lib/pytis/extensions/dmp.py
@@ -984,7 +984,9 @@ class DMPActions(DMPObject):
                                     "Failed to retrieve RUN_FORM command", (form_string,))
                     form_name = None
                 if form_name is not None:
-                    self._alternate_fullname = 'form/%s/%s//' % (form_class, form_name,)
+                    self._alternate_fullname = ('form/%s.%s/%s//' %
+                                                (form_class.__module__, form_class.__name__,
+                                                 form_name,))
                 else:
                     self._alternate_fullname = fullname
             elif components[0] == 'sub' and components[2] == 'RUN_FORM':
