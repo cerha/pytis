cerha/pytis

commit 0cd44f89aae633347bbacd57619a9c9906e6986c

Prevent ListForm._on_idle from blocking edit forms during row counting

lib/pytis/form/application.py
@@ -812,14 +812,18 @@ class Application(wx.App, KeyHandler, CommandHandler):
                     return None
             log(ACTION, 'Vytváøím nový formuláø:', (form_class, name, kwargs))
             message(_("Spou¹tím formuláø..."), root=True)
-            wx_yield_()
             assert issubclass(form_class, Form)
             assert is_anystring(name)
+            # We indicate busy state here so that the action is not delayed by
+            # some time consuming _on_idle methods.
+            busy_cursor(True)
+            wx_yield_()
             result = None
             self.save()
             form = find((form_class, name), self._windows.items(),
                         key=lambda f: (f.__class__, f.name()))
             if form is not None:
+                busy_cursor(False)
                 self._raise_form(form)
                 message(_('Formuláø "%s" nalezen na zásobníku oken.') % form.title())
                 if kwargs.has_key('select_row'):
@@ -842,6 +846,7 @@ class Application(wx.App, KeyHandler, CommandHandler):
             except Form.InitError:
                 form = None
             if form is None:
+                busy_cursor(False)
                 self.run_dialog(Error, _("Formuláø se nepodaøilo vytvoøit: %s") % name)
             else:
                 if isinstance(form, PopupForm):

lib/pytis/form/list.py
@@ -851,6 +851,8 @@ class ListForm(RecordForm, TitledForm, Refreshable):
         self._show_data_status()
 
     def _on_idle(self, event):
+        if is_busy_cursor():
+            return False
         if self._selection_candidate is not None:
             row, col = self._selection_candidate
             self._selection_candidate = None

commit 6ae979749650f711f2baaa9510102f73c012621c

Fix inaccurate initial scrolling position of codebook forms.
The problem was introduced by commit 0cd44f89aae633347bbacd57619a9c9906e6986c
"Prevent ListForm._on_idle from blocking edit forms during row counting".  The
busy cursor was not released before running popup forms, which prevented
invocation of the _on_idle method of the codebook form.

lib/pytis/form/application.py
@@ -854,6 +854,7 @@ class Application(wx.App, KeyHandler, CommandHandler):
                     self._modals.push(form)
                     message('', root=True)
                     form.show()
+                    busy_cursor(False)
                     try:
                         form_str = str(form) # Dead form doesn't speak...
                         result = form.run()