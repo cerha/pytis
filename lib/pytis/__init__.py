3d191de3f5553b533a35330bc6fd4b6fa59cfdec
Tomas Cerha <cerha@brailcom.org>
2010-11-22 18:20:04 +0100

    Released version 1.1.0

--- lib/pytis/__init__.py
+++ lib/pytis/__init__.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 import util
 import data
