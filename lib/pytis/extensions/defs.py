e6cb44f28b686982257ca5c9a052d90767a1ddd9
Milan Zamazal <pdm@brailcom.org>
2011-05-05 12:40:30 +0200

    Use unicode literals in defs.py

--- lib/pytis/extensions/defs.py
+++ lib/pytis/extensions/defs.py
@@ -16,6 +16,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+from __future__ import unicode_literals
+
 """Funkce pro načítání, caching, kontrolu a reporty z defsů.""" 
 
 import pytis.data
