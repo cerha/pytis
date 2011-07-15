51c258e7875a4baf1533fa37726ec63b7fef3408
Tomas Cerha <cerha@brailcom.org>
2011-07-15 17:39:09 +0200

    Use separate sets of profiles for different aggregated view configurations

--- lib/pytis/form/list.py
+++ lib/pytis/form/list.py
@@ -3140,6 +3140,11 @@ class AggregationForm(BrowseForm):
         self._data_kwargs['condition'] = self._af_aggregation_condition
         return ViewSpec(view.title(), fields)
 
+    def _fullname(self):
+        # We need to have unique fullnames for different column configurations
+        # because profiles for one configuration may not be valid in the other.
+        return super(AggregationForm, self)._fullname() + ':'.join(self._select_columns())
+    
     def _can_aggregated_view(self, aggregated_view_id):
         return False
 
