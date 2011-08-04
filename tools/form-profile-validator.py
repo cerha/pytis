e0f2a210657919b15aa6d116219b2b3eb7294080
Tomas Cerha <cerha@brailcom.org>
2011-08-04 10:20:45 +0200

    Omit unnecessary condition in script tools/form-profile-validator.py
    There are no special profile ids now (the DB conversion script should ensure
    that).

--- tools/form-profile-validator.py
+++ tools/form-profile-validator.py
@@ -74,38 +74,32 @@ def run():
         for fullname in manager.list_fullnames():
             specname = fullname.split('/')[2]
             for profile_id in manager.list_profile_ids(fullname):
-                if profile_id not in ('__dualform__', '__form_settings__'):
-                    # '__dualform__' profiles are no longer used, but they may
-                    # remain in the database. once they are deleted from all
-                    # projects, the condition may be removed.
-                    # '__form_settings__' profiles are not FormProfile
-                    # instances and they don't need to be validated.
-                    profile = manager.load_profile(fullname, profile_id)
+                profile = manager.load_profile(fullname, profile_id)
+                try:
+                    view_spec, data_object, error = cache[specname]
+                except KeyError:
+                    resolver = pytis.util.resolver()
                     try:
-                        view_spec, data_object, error = cache[specname]
-                    except KeyError:
-                        resolver = pytis.util.resolver()
-                        try:
-                            view_spec = resolver.get(specname, 'view_spec')
-                            data_spec = resolver.get(specname, 'data_spec')
-                            data_object = data_spec.create(dbconnection_spec=config.dbconnection)
-                        except Exception as e:
-                            print "%s: %s" % (specname, e)
-                            view_spec, data_object,  error = None, None, e
-                        else:
-                            error = None
-                        cache[specname] = view_spec, data_object,  error
-                    if not error:
-                        errors = profile.validate(view_spec, data_object)
-                        # Update the 'errors' column in the database table.
-                        manager.save_profile(fullname, profile)
-                        for error in errors:
-                            print ':'.join((username, fullname, profile_id, ' '+ error))
-                        if errors:
-                            total_invalid += 1
-                        else:
-                            total_valid += 1
-                    total_processed += 1
+                        view_spec = resolver.get(specname, 'view_spec')
+                        data_spec = resolver.get(specname, 'data_spec')
+                        data_object = data_spec.create(dbconnection_spec=config.dbconnection)
+                    except Exception as e:
+                        print "%s: %s" % (specname, e)
+                        view_spec, data_object,  error = None, None, e
+                    else:
+                        error = None
+                    cache[specname] = view_spec, data_object,  error
+                if not error:
+                    errors = profile.validate(view_spec, data_object)
+                    # Update the 'errors' column in the database table.
+                    manager.save_profile(fullname, profile)
+                    for error in errors:
+                        print ':'.join((username, fullname, profile_id, ' '+ error))
+                    if errors:
+                        total_invalid += 1
+                    else:
+                        total_valid += 1
+                total_processed += 1
     print (u"Total %d profiles processed:\n"
            u"  %d valid\n"
            u"  %d invalid") % (total_processed, total_valid, total_invalid)
