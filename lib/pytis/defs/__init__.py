040dcecfc7b2cabbc183b8fbf178812fca7878ee
Tomas Cerha <cerha@brailcom.org>
2011-09-01 08:41:13 +0200

    Make pytis specifications part of the `pytis' python library
    This makes the specifications available through the new resolver.

---
+++ lib/pytis/defs/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2001, 2002, 2006, 2010, 2011 Brailcom, o.p.s.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""Pytis specification files.
+
+Pytis ships with its own set of specifications which may be used in
+applications to access pytis system data through the user interface.
+
+"""
+
+import menu
+import printing
+import profiles
+import statistics
