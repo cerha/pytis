63d28e087501cc7578810b83e4275bce3d416468
Tomas Cerha <cerha@brailcom.org>
2012-07-24 11:43:17 +0200

    Add management of help for fields, bindings, actions and profiles

--- lib/pytis/defs/help.py
+++ lib/pytis/defs/help.py
@@ -17,10 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import pytis.data as pd, pytis.presentation as pp
+import pytis.data as pd, pytis.presentation as pp, pytis.util
 from pytis.presentation import Specification, Field, CodebookSpec, Editable, HGroup, Profile, \
     Binding, computer
-from pytis.util import parse_lcg_text
 
 class _TreeOrderLTree(pp.PrettyFoldable, pd.String):
     pass
@@ -99,7 +98,7 @@ class Help(Specification):
                                for f in ('menu_help', 'spec_description', 'spec_help', 'content')
                                if record[f].value() is not None])
         storage = self._attachment_storage(record)
-        return parse_lcg_text(content, resources=storage and storage.resources() or ())
+        return pytis.util.parse_lcg_text(content, resources=storage and storage.resources() or ())
         
     def redirect(self, record):
         if record['page_id'].value() is not None:
@@ -113,8 +112,11 @@ class Help(Specification):
     def bindings(self):
         return (
             Binding('content', _("Obsah"), content=self._content),
+            Binding('fields', _("Políčka"), 'help.FieldItemsHelp', 'spec_name'),
+            Binding('profiles', _("Profily"), 'help.ProfileItemsHelp', 'spec_name'),
+            Binding('actions', _("Akce"), 'help.ActionItemsHelp', 'spec_name'),
+            Binding('bindings', _("Vedlejší formuláře"), 'help.BindingItemsHelp', 'spec_name'),
             )
-            
 
     layout = ('title', 'description', 'parent', 'ord', 'content')
     cb = CodebookSpec(display='title')
@@ -129,5 +131,64 @@ class SpecHelp(Help):
 class MenuHelp(Help):
     layout = ('title', 'fullname', 'menu_help')
 
+    
 class NoHelp(Help):
     layout = ('title', pp.Text(_(u"Tato položka nemá editovatelnou nápovědu.")))
+
+
+class ItemsHelp(Specification):
+    _ITEM_KIND = None
+    public = True
+    table = 'e_pytis_help_spec_items'
+    def fields(self):
+        return (
+            Field('item_id'),
+            Field('kind'),
+            Field('spec_name', codebook='help.Help', value_column='spec_name'),
+            Field('identifier', _(u"Identifikátor"), width=30, editable=Editable.NEVER),
+            Field('content', _(u"Popis"), width=80, height=15, compact=True,
+                  text_format=pp.TextFormat.LCG, attachment_storage=self._attachment_storage),
+            Field('label', _(u"Název"), width=30, virtual=True, computer=computer(self._label)),
+            )
+    def _attachment_storage(self, record):
+        return pp.DbAttachmentStorage('e_pytis_help_spec_attachments', 'spec_name',
+                                      record['spec_name'].value(), base_uri='resource:')
+    def _label(self, record, spec_name, kind, identifier):
+        resolver = pytis.util.resolver()
+        try:
+            view_spec = resolver.get(spec_name, 'view_spec')
+        except pytis.util.ResolverError:
+            return identifier
+        items = getattr(view_spec, kind+'s')()
+        item = pytis.util.find(identifier, items, key=lambda x: x.id())
+        if item:
+            if kind in ('field',):
+                return item.label()
+            elif kind in ('action', 'binding'):
+                return item.title()
+            else:
+                return item.name()
+        else:
+            return identifier
+        
+    def condition(self):
+        return pd.EQ('kind', pd.sval(self._ITEM_KIND))
+    columns = ('identifier', 'label')
+    layout = ('identifier', 'label', 'content')
+
+
+class FieldItemsHelp(ItemsHelp):
+    _ITEM_KIND = 'field'
+
+
+class BindingItemsHelp(ItemsHelp):
+    _ITEM_KIND = 'binding'
+
+
+class ProfileItemsHelp(ItemsHelp):
+    _ITEM_KIND = 'profile'
+
+
+class ActionItemsHelp(ItemsHelp):
+    _ITEM_KIND = 'action'
+    
