87b758c750a323f2150a6615326f7f87d9038093
Milan Zamazal <pdm@brailcom.org>
2012-11-20 10:53:43 +0100

    Use separate pytisproc module in windows-server

---
+++ lib/pytis/windows/pytisproc.py
@@ -0,0 +1,364 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011, 2012 Brailcom, o.p.s.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# ATTENTION: This should be updated on each code change.
+_VERSION = '2012-11-20 09:31'
+
+import os
+import rpyc
+import sys
+import tempfile
+import time
+
+class PytisService(rpyc.Service):
+
+    registration = None
+
+    def exposed_echo(self, text):
+        """Return 'text'.
+
+        This is useful for checking whether the service is working.
+        
+        """
+        return text
+
+    def exposed_version(self):
+        """Return server version."""
+        return _VERSION
+
+    def exposed_run_python(self, script):
+        """Run Python on given script.
+
+        Arguments:
+
+          script -- source text of the Python script to run
+
+        There is no way to get any output from the script here.  If you need to
+        get some information from the script then you must handle it in the
+        script itself, e.g. by storing it to some file.
+
+        """
+        assert isinstance(script, basestring), script
+        pythonw = sys.executable
+        tmpdir = tempfile.mkdtemp(prefix='pytisexec')
+        try:
+            python_file = os.path.join(tmpdir, 'script.py')
+            try:
+                open(python_file, 'wb').write(script)
+                import win32api
+                win32api.WinExec('%s %s' % (pythonw, python_file,))
+            finally:
+                os.remove(python_file)
+        finally:
+            try:
+                os.rmdir(tmpdir)
+            except:
+                pass
+
+class PytisUserService(PytisService):
+
+    def exposed_restart(self):
+        """Restart the user service."""
+        file_name = sys.argv[0]
+        if file_name[:-4] == '.pyc':
+            file_name = file_name[:-1]
+        execfile(file_name)
+        
+    def exposed_get_clipboard_text(self):
+        """Return current clipboard text, as unicode.
+
+        If the text can't be retrieved, return 'None'.
+        
+        """
+        import win32clipboard
+        win32clipboard.OpenClipboard()
+        try:
+            data = win32clipboard.GetClipboardData(win32clipboard.CF_UNICODETEXT)
+        except:                   # may happen when there is no clipboard data
+            data = None
+        win32clipboard.CloseClipboard()
+        return data
+        
+    def exposed_set_clipboard_text(self, text):
+        """Set clipboard content to text.
+
+        Arguments:
+
+          text -- text to store into the clipboard; unicode
+          
+        """
+        assert isinstance(text, unicode), text
+        import win32clipboard
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardData(win32clipboard.CF_UNICODETEXT, text)
+        win32clipboard.CloseClipboard()
+
+    def exposed_launch_file(self, path):
+        """Start associated application on path.
+
+        Arguments:
+
+          path -- path to the file to be started with its associated
+            application; basestring.  Note that path must be given in the
+            Windows form, i.e. use backslashes to separate path elements.
+
+        """
+        assert isinstance(path, basestring), path
+        os.startfile(path)
+
+    def exposed_open_selected_file(self, template=None):
+        """Return a read-only 'file' like object of a user selected file.
+
+        The file is selected by the user using a GUI dialog.  If the user
+        cancels the dialog, 'None' is returned.
+
+        Arguments:
+
+          template -- a string defining the required file name pattern, or 'None'
+
+        """
+        assert template is None or isinstance(template, basestring), template
+        import win32ui
+        import win32con
+        if template:
+            file_filter = (u"Soubory požadovaného typu (%s)|%s|Všechny soubory (*.*)|*.*||" %
+                           (template, template,))
+            filename = template
+            if template.find('.') != -1:
+                extension = template.split('.')[-1]
+        else:
+            file_filter = u"Všechny soubory (*.*)|*.*||"
+            filename = "*.*"
+        parent = win32ui.FindWindow(None,None)
+        dialog = win32ui.CreateFileDialog(1, extension, "%s" % filename,
+                                          win32con.OFN_HIDEREADONLY|win32con.OFN_OVERWRITEPROMPT,
+                                          file_filter, parent)
+        result = dialog.DoModal()
+        if result != 1:
+            return None
+        filename = dialog.GetPathName()
+        if filename is None:
+            return None
+        class Wrapper(object):
+            def __init__(self, filename):
+                self._f = open(filename, 'rb')
+                self._filename = filename
+            def exposed_read(self):
+                return self._f.read()
+            def exposed_close(self):
+                self._f.close()
+            def exposed_name(self):
+                return self._filename
+        return Wrapper(filename)
+
+    def exposed_make_selected_file(self, filename=None, template=None, encoding=None, mode='wb'):
+        """Return a write-only 'file' like object of a user selected file.
+
+        The file is selected by the user using a GUI dialog.  If the user
+        cancels the dialog, 'None' is returned.
+
+        Arguments:
+
+          filename -- default filename or None
+          template -- a string defining the required file name pattern, or 'None'
+          encoding -- output encoding, string or None
+          mode -- default mode for opening the file 
+
+        """
+        assert template is None or isinstance(template, basestring), template
+        import win32ui
+        import win32con
+        file_filter = u"Všechny soubory (*.*)|*.*||"
+        extension = None
+        if filename:
+            name, ext = os.path.splitext(filename)
+            if ext:
+                template = "*" + ext
+                file_filter = (u"Soubory požadovaného typu (%s)|%s|%s" %
+                               (template, template, file_filter))
+                extension = ext[1:]
+        else:
+            filename = "*.*"
+            if template:
+                file_filter = (u"Soubory požadovaného typu (%s)|%s|%s" %
+                               (template, template, file_filter))
+        # This hack with finding non-specified windows is used so that
+        # we get some parent window for CreateFileDialog.
+        # Without this parent windows the method DoModal doesn't show
+        # the dialog window on top...
+        parent = win32ui.FindWindow(None,None) 
+        dialog = win32ui.CreateFileDialog(0, extension, "%s" % filename,
+                                          win32con.OFN_HIDEREADONLY|win32con.OFN_OVERWRITEPROMPT,
+                                          file_filter, parent)
+        result = dialog.DoModal()
+        if result != 1:
+            return None
+        filename = dialog.GetPathName()
+        if filename is None:
+            return None
+        class Wrapper(object):
+            def __init__(self, filename, encoding, mode):
+                self._f = open(filename, mode)
+                self._filename = filename
+                self._encoding = encoding
+            def exposed_write(self, data):
+                if isinstance(data, buffer):
+                    data = data[:]
+                elif self._encoding is not None:
+                    data = data.encode(self._encoding)
+                self._f.write(data)
+            def exposed_close(self):
+                self._f.close()
+            def exposed_name(self):
+                return self._filename
+        return Wrapper(filename, encoding, mode)
+
+    def exposed_make_temporary_file(self, suffix='', encoding=None, mode='wb'):
+        """Create a temporary file and return its instance.
+
+        The return value is a 'tempfile.NamedTemporaryFile' instance.
+
+        Arguments:
+
+          suffix -- suffix to use in the temporary file name; if a dot should
+            be part of the suffix then it must be explicitly included in it
+          encoding -- output encoding, string or None
+          mode -- default mode for opening the file 
+        
+        """
+        class Wrapper(object):
+            def __init__(self, handle, filename, encoding, mode):
+                self._f = os.fdopen(handle, mode)
+                self._filename = filename
+                self._encoding = encoding
+            def exposed_write(self, data):
+                if isinstance(data, buffer):
+                    data = data[:]
+                elif self._encoding is not None:
+                    data = data.encode(self._encoding)
+                self._f.write(data)
+            def exposed_close(self):
+                self._f.close()
+            def exposed_name(self):
+                return self._filename
+        handle, filename = tempfile.mkstemp(prefix='pytis', suffix=suffix)
+        return Wrapper(handle, filename, encoding, mode)
+
+class PytisAdminService(PytisService):
+
+    def _true_authentication(self):
+        # Dirty: We access private attribute of self._conn.  But how to access
+        # credentials otherwise?  And RPyC's SlaveService accesses the
+        # attribute too.
+        return self._conn._config.get('credentials')
+
+    def exposed_restart(self, delay=1):
+        """Restart the admin service after given timeout.
+
+        Arguments:
+
+          delay -- number of minutes to wait before the server gets restarted
+
+        """
+        import pythoncom
+        from win32com.taskscheduler import taskscheduler
+        scheduler = pythoncom.CoCreateInstance(taskscheduler.CLSID_CTaskScheduler,
+                                               None,
+                                               pythoncom.CLSCTX_INPROC_SERVER,
+                                               taskscheduler.IID_ITaskScheduler)
+        def add_task(name, command, run_time):
+            if '%s.job' % (name,) in scheduler.Enum():
+                scheduler.Delete(name)
+            task = scheduler.NewWorkItem(name)
+            cmd = command.split()
+            task.SetApplicationName(cmd[0])
+            task.SetParameters(' '.join(cmd[1:]))
+            task.SetPriority(taskscheduler.REALTIME_PRIORITY_CLASS)
+            task.SetFlags(taskscheduler.TASK_FLAG_DELETE_WHEN_DONE|
+                          taskscheduler.TASK_FLAG_RUN_ONLY_IF_LOGGED_ON)
+            task.SetAccountInformation('', None)
+            scheduler.AddWorkItem(name, task)
+            __init__, task_trigger = task.CreateTrigger()
+            trigger = task_trigger.GetTrigger()
+            trigger.Flags = 0
+            time_tupple = time.localtime(run_time)
+            trigger.BeginYear = time_tupple[0]
+            trigger.BeginMonth = time_tupple[1]
+            trigger.BeginDay = time_tupple[2]
+            trigger.StartHour = time_tupple[3]
+            trigger.StartMinute = time_tupple[4]
+            trigger.TriggerType = int(taskscheduler.TASK_TIME_TRIGGER_ONCE)
+            task_trigger.SetTrigger(trigger)
+            persist_file = task.QueryInterface(pythoncom.IID_IPersistFile)
+            persist_file.Save(None, 1)
+            task = scheduler.Activate(name)
+        now = time.time()
+        add_task('pytis_service_stop', 'sc stop PytisService', now + delay*60)
+        add_task('pytis_service_start', 'sc start PytisService', now + (delay+1)*60)
+        
+    def exposed_register_user(self, user, port):
+        assert isinstance(user, basestring), user
+        assert isinstance(port, int), port
+        self._registration.registered_users[user] = port
+
+    def exposed_user_port(self, user):
+        assert isinstance(user, basestring), user
+        if user == '-':
+            port = None
+            for p in self._registration.registered_users.values():
+                if port is None or port > p:
+                    port = p
+        else:
+            port = self._registration.registered_users.get(user)
+        return port
+
+    def exposed_upgrade(self, source):
+        assert source and isinstance(source, basestring), source
+        if not self._true_authentication():
+            raise Exception("Permission denied")
+        file_name = __file__
+        file_name = os.path.join(os.path.dirname(file_name), 'windows_server.py')
+        self._upgrade(file_name, source)
+        file_name = os.path.join(os.path.dirname(file_name), 'windows_user_server.py')
+        self._upgrade(file_name, source)
+
+    def exposed_upgrade_module(self, source):
+        assert source and isinstance(source, basestring), source
+        if not self._true_authentication():
+            raise Exception("Permission denied")
+        file_name = __file__
+        file_name = os.path.join(os.path.dirname(file_name), 'pytisproc.py')
+        self._upgrade(file_name, source)
+
+    def _upgrade(self, file_name, source):
+        new_file_name = file_name + '.new'
+        f = open(new_file_name, 'wb')
+        f.write(source)
+        f.close()
+        for suffix in ('c', 'o',):
+            try:
+                os.remove(file_name + suffix)
+            except OSError:
+                pass
+        os.remove(file_name)
+        os.rename(new_file_name, file_name)
+
+# Local Variables:
+# time-stamp-pattern: "30/^_VERSION = '%Y-%02m-%02d %02H:%02M'"
+# End:
