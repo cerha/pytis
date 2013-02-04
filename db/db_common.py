0e81d2d39dd2efb5464ea3382cdcd83bda1c8308
Milan Zamazal <pdm@brailcom.org>
2013-02-04 16:34:22 +0100

    Additional arguments to f_view_log

--- db/db_common.py
+++ db/db_common.py
@@ -864,19 +864,33 @@ begin
 end;
 $$ language plpgsql;
 
-create or replace function f_view_log(date_from date, date_to date) returns setof v_changes as $$
+create or replace function f_view_log(date_from date, date_to date, username_ text, tablename_ text, key_value_ text, detail_ text) returns setof v_changes as $$
 declare
   date_to_1 date := date_to + '1 day'::interval;
   tablename text;
 begin
-  return query select * from v_changes where date_from <= timestamp and timestamp < date_to_1;
+  return query select * from v_changes v
+                      where date_from <= v.timestamp and v.timestamp < date_to_1 and
+                            v.username = coalesce(username_, v.username) and
+                            v.tablename = coalesce(tablename_, v.tablename) and
+                            v.key_value::text = coalesce(key_value_, v.key_value) and
+                            v.detail like coalesce(detail_, '%');
   for tablename in select relname from pg_class join pg_namespace on relnamespace = pg_namespace.oid
                           where nspname = 'public' and
                                 relname ~ '^t_changes_......_......$' and
                                 substring(relname from 11 for 6) <= to_char(date_to, 'YYMMDD') and
                                 substring(relname from 18 for 6) >= to_char(date_from, 'YYMMDD')
   loop
-    return query execute concat('select t.*, detail from public.', tablename, ' t join public.t_changes_detail_', substring(tablename from 11), ' using(id) where $1 <= timestamp and timestamp < $2') using date_from, date_to_1;
+    return query
+      execute concat('select t.*, detail ',
+                             'from public.', tablename, ' t join ',
+                             'public.t_changes_detail_', substring(tablename from 11), ' d using(id) ',
+                             'where $1 <= t.timestamp and t.timestamp < $2 and ',
+                                   't.username = coalesce($3, t.username) and ',
+                                   't.tablename = coalesce($4, t.tablename) and ',
+                                   't.key_value::text = coalesce($5, t.key_value) and ',
+                                   'd.detail like coalesce($6, ''%'')')
+              using date_from, date_to_1, username_, tablename_, key_value_, detail_;
   end loop;
 end;
 $$ language plpgsql stable;
