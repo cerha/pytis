b45096466adc503a8e7c2f931bb224922ac6c2d3
Milan Zamazal <pdm@brailcom.org>
2013-05-07 10:43:52 +0200

    Use unicode literals in some modules

--- lib/pytis/defs/profiles.py
+++ lib/pytis/defs/profiles.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import unicode_literals
+
 import pytis.data as pd, pytis.util
 from pytis.presentation import Specification, Field, CodebookSpec, Editable, HGroup, Profile, \
     computer
