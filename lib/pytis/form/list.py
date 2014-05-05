43fbeb85c4eb4b66fbf15445dd462f0a1bc7417d
Tomas Cerha <cerha@brailcom.org>
2014-05-05 11:35:41 +0200

    Use ActionGroup.items() instead of old ActionGroup.actions()

--- lib/pytis/form/list.py
+++ lib/pytis/form/list.py
@@ -3164,7 +3164,7 @@ class BrowseForm(FoldableForm):
                 cmd = self.COMMAND_CONTEXT_ACTION(action=x)
                 items.append(MItem(x.title(raw=True), command=cmd, help=x.descr()))
             elif isinstance(x, ActionGroup):
-                items.append(Menu(x.title(), self._action_mitems(x.actions())))
+                items.append(Menu(x.title(), self._action_mitems(x.items())))
             elif isinstance(x, (types.TupleType, types.ListType)):
                 if items:
                     items.append(MSeparator())
