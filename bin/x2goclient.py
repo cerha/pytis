495b3a1ff8f9b8a0931ee94fcd50f31d8b3b61e6
Milan Zamazal <pdm@brailcom.org>
2015-01-14 15:33:10 +0100

    Put x2goclient.py under GNU AGPL
    This is to safely comply with python-x2go and pyhoca.

--- bin/x2goclient.py
+++ bin/x2goclient.py
@@ -4,23 +4,22 @@
 # Copyright (C) 2011, 2012, 2014, 2015 Brailcom, o.p.s.
 #
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
 
 # ATTENTION: This should be updated on each code change.
-_VERSION = '2015-01-12 15:47'
+_VERSION = '2015-01-14 15:32'
 
 import gevent.monkey
 gevent.monkey.patch_all()
