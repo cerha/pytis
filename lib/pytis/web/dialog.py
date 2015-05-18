2d2e1f3c4d6d84b37962d9745863bd35cfbe5e80
Tomas Cerha <cerha@brailcom.org>
2015-05-18 11:51:40 +0200

    Work around tiny fonts of some form buttons in some browsers

--- lib/pytis/web/dialog.py
+++ lib/pytis/web/dialog.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2007, 2008, 2011, 2013, 2014 Brailcom, o.p.s.
+# Copyright (C) 2007, 2008, 2011, 2013, 2014, 2015 Brailcom, o.p.s.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -48,7 +48,8 @@ class Dialog(lcg.Content):
         if self._action:
             hidden += [('action', self._action)]
         # Translators: Default edit form submit button label.
-        content += tuple([g.hidden(k, v) for k, v in hidden]) + (g.submit(_(u"Submit")),)
+        content += tuple([g.hidden(k, v) for k, v in hidden] +
+                         [g.button(g.span(_(u"Submit")), type='submit')])
         return g.form(content, action=self._handler, cls="dialog", method="POST") + "\n"
 
 
