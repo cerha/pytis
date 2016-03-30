1ea4a5e1bda7c59ed0643646ece66fd74a450bf0
Tomas Cerha <cerha@brailcom.org>
2016-03-30 18:13:23 +0200

    Make ExposedFileWrapper tests work on Windows

--- lib/pytis/remote/_test.py
+++ lib/pytis/remote/_test.py
@@ -87,57 +87,73 @@ class FileWrapper(unittest.TestCase):
         return data.decode('base64')
 
     def _test_read(self, filename, data, mode='r', **kwargs):
-        try:
-            with file(filename, 'w') as f:
-                f.write(data)
-            wrapper = pytis.remote.pytisproc.ExposedFileWrapper(filename, mode=mode, **kwargs)
-            data2 = wrapper.exposed_read()
-            if 'encrypt' in kwargs:
-                data2 = self._decrypt(data2)
-            wrapper.exposed_close()
-            self.assertEqual(data2, data)
-        finally:
-            os.unlink(filename)
+        with file(filename, 'w') as f:
+            f.write(data)
+        wrapper = pytis.remote.pytisproc.ExposedFileWrapper(filename, mode=mode, **kwargs)
+        data2 = wrapper.exposed_read()
+        if 'encrypt' in kwargs:
+            data2 = self._decrypt(data2)
+        wrapper.exposed_close()
+        self.assertEqual(data2, data)
 
     def _test_write(self, filename, data, mode='w', **kwargs):
-        try:
-            wrapper = pytis.remote.pytisproc.ExposedFileWrapper(filename, mode=mode, **kwargs)
-            if 'decrypt' in kwargs:
-                data_to_write = self._encrypt((StringIO.StringIO(data)))
-            else:
-                data_to_write = data
-            wrapper.exposed_write(data_to_write)
-            wrapper.exposed_flush()
-            wrapper.exposed_close()
-            with file(filename) as f:
-                data2 = f.read()
-            self.assertEqual(data2, data)
-        finally:
-            os.unlink(filename)
+        wrapper = pytis.remote.pytisproc.ExposedFileWrapper(filename, mode=mode, **kwargs)
+        if 'decrypt' in kwargs:
+            data_to_write = self._encrypt((StringIO.StringIO(data)))
+        else:
+            data_to_write = data
+        wrapper.exposed_write(data_to_write)
+        wrapper.exposed_flush()
+        wrapper.exposed_close()
+        with file(filename) as f:
+            data2 = f.read()
+        self.assertEqual(data2, data)
 
     def test_read(self):
-        filename = tempfile.mkstemp(prefix='pytistmp', suffix='txt')[1]
-        self._test_read(filename, "foo('bar')='baz'")
+        fd, filename = tempfile.mkstemp(prefix='pytistmp', suffix='.txt')
+        try:
+            self._test_read(filename, "foo('bar')='baz'")
+        finally:
+            os.close(fd)
+            os.remove(filename)
 
     def test_read_handle(self):
-        handle, filename = tempfile.mkstemp(prefix='pytistmp', suffix='txt')
-        self._test_read(filename, "foo('bar')='baz'", handle=handle)
+        fd, filename = tempfile.mkstemp(prefix='pytistmp', suffix='.txt')
+        try:
+            self._test_read(filename, "foo('bar')='baz'", handle=fd)
+        finally:
+            os.remove(filename)
 
     def test_encryption(self):
-        filename = tempfile.mkstemp(prefix='pytistmp', suffix='txt')[1]
-        self._test_read(filename, "foo('bar')='baz'", encrypt=self._encrypt)
+        fd, filename = tempfile.mkstemp(prefix='pytistmp', suffix='.txt')
+        try:
+            self._test_read(filename, "foo('bar')='baz'", encrypt=self._encrypt)
+        finally:
+            os.close(fd)
+            os.remove(filename)
 
     def test_write(self):
-        filename = tempfile.mkstemp(prefix='pytistmp', suffix='txt')[1]
-        self._test_write(filename, "foo('bar')='baz'")
+        fd, filename = tempfile.mkstemp(prefix='pytistmp', suffix='.txt')
+        try:
+            self._test_write(filename, "foo('bar')='baz'")
+        finally:
+            os.close(fd)
+            os.remove(filename)
 
     def test_write_handle(self):
-        handle, filename = tempfile.mkstemp(prefix='pytistmp', suffix='txt')
-        self._test_write(filename, "foo('bar')='baz'", handle=handle)
+        fd, filename = tempfile.mkstemp(prefix='pytistmp', suffix='.txt')
+        try:
+            self._test_write(filename, "foo('bar')='baz'", handle=fd)
+        finally:
+            os.remove(filename)
 
     def test_decryption(self):
-        filename = tempfile.mkstemp(prefix='pytistmp', suffix='txt')[1]
-        self._test_write(filename, "foo('bar')='baz'", decrypt=self._decrypt)
+        fd, filename = tempfile.mkstemp(prefix='pytistmp', suffix='.txt')
+        try:
+            self._test_write(filename, "foo('bar')='baz'", decrypt=self._decrypt)
+        finally:
+            os.close(fd)
+            os.remove(filename)
 
 
 if __name__ == '__main__':
