9d92420873d371cddc82bd8cb3c6e3083e201e66
Pavel Hanak <hanak@is-it.eu>
2016-04-05 22:45:02 +0200

    Update timestamp of x2goclient.py.

--- bin/x2goclient.py
+++ bin/x2goclient.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # ATTENTION: This should be updated on each code change.
-_VERSION = '2016-03-03 16:51'
+_VERSION = '2016-04-05 22:44'
 
 XSERVER_VARIANTS = ('VcXsrv_pytis', 'VcXsrv_pytis_desktop')
 XSERVER_VARIANT_DEFAULT = 'VcXsrv_pytis'
