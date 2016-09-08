cerha/pytis

commit c4a50e1d26828eee1315bab36a61869ac3485861

Use literal_column instead of TextClause when calling SQLFunction.

lib/pytis/data/gensqlalchemy.py
@@ -3313,7 +3313,8 @@ class SQLFunctional(_SQLReplaceable, _SQLTabular):
         # argument list.
         argument_list = [unicode(_sql_value_escape(a)) for a in arguments]
         expression = u'%s(%s)' % (name, string.join(argument_list, ', '),)
-        return sqlalchemy.sql.expression.TextClause(expression)
+        return sqlalchemy.sql.expression.literal_column(expression,
+                                                        type_=self.result_type.sqlalchemy_type())
 
     def body(self):
         """Return function body as basestring.

commit a08d406efe5288acfb94144670109abbf052fd26

Improve previous gsql failure fix
This fix is based on finding the regression in commit
c4a50e1d26828eee1315bab36a61869ac3485861 by PH and restoring the
previous behavior for given corner case.  Thus should be safe.

lib/pytis/data/gensqlalchemy.py
@@ -3314,14 +3314,14 @@ class SQLFunctional(_SQLReplaceable, _SQLTabular):
         argument_list = [unicode(_sql_value_escape(a)) for a in arguments]
         expression = u'%s(%s)' % (name, string.join(argument_list, ', '),)
         result_type = self.result_type
-        if result_type is G_CONVERT_THIS_FUNCTION_TO_TRIGGER or \
-           isinstance(result_type, (tuple, list,)) or result_type == self.RECORD:
+        if ((result_type is None or
+             result_type is G_CONVERT_THIS_FUNCTION_TO_TRIGGER or
+             result_type == self.RECORD or
+             isinstance(result_type, (tuple, list,)))):
             return sqlalchemy.sql.expression.TextClause(expression)
         else:
-            return sqlalchemy.sql.expression.literal_column(
-                expression,
-                type_=result_type.sqlalchemy_type() if result_type else None
-            )
+            return sqlalchemy.sql.expression.literal_column(expression,
+                                                            type_=result_type.sqlalchemy_type())
 
     def body(self):
         """Return function body as basestring.