cerha/pytis

commit be24069e74e6e589805592bc61cbb48d1b68baca

Don't convert binary values to buffer in _PgValue

lib/pytis/data/postgresql.py
@@ -112,14 +112,8 @@ class _PgValue(object):
     def _convert_value(self, value):
         v = value.value()
         t = value.type()
-        if v is None:
-            result = None
-        elif isinstance(t, Array):
+        if isinstance(t, Array) and v is not None:
             result = [vv.value() for vv in v]
-        elif isinstance(t, Binary):
-            result = buffer(v)
-            if not str(result):
-                result = None
         else:
             result = v
         return result

commit 03b82f132251ed83ba145082db7474a6d2571006

Fix passing binary values to DB function calls
Fixes problem caused by commit be24069e "Don't convert binary values to buffer
in _PgValue".  That commit didn't consider backwards compatibility with Python
2.

lib/pytis/data/postgresql.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2018-2019 Tomáš Cerha <t.cerha@gmail.com>
+# Copyright (C) 2018-2020 Tomáš Cerha <t.cerha@gmail.com>
 # Copyright (C) 2001-2017 OUI Technology Ltd.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -114,6 +114,11 @@ class _PgValue(object):
         t = value.type()
         if isinstance(t, Array) and v is not None:
             result = [vv.value() for vv in v]
+        elif isinstance(t, Binary) and v is not None:
+            # TODO: This is actually only necessary in Python 2.  Psycopg2 seems
+            # to accept bytes in Python 3 without problems so this may not be
+            # necessary.
+            result = memoryview(v)
         else:
             result = v
         return result