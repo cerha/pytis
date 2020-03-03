e22ab1067af178e50aa713e8ac3c267c1fad30b7
Tomáš Cerha <t.cerha@gmail.com>
2020-03-03 07:52:56 +0100

    Remove unused postscript viewer code

--- lib/pytis/form/__init__.py
+++ lib/pytis/form/__init__.py
@@ -88,7 +88,7 @@ from .dualform import (  # noqa: F401
 )
 
 from .output import (  # noqa: F401
-    PostscriptException, PostscriptViewer, PrintForm, PrintFormExternal,
+    PrintForm, PrintFormExternal,
 )
 
 from .application import (  # noqa: F401
