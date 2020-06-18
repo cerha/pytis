cerha/pytis

commit 660f5066aa3a2f315f290a726c620b8c2861704c

Never show data before query fields are applied
Avoid showing any data before query fields are properly applied when autoinit &
autoapply is False.  Make sure to also do it when neither condition_provider
nor argument_provider is defined.  This may be necessary when query fields are
used for example only to influence the side form condition.

Needs some testing as we are changing some sensitive details of form
initialization and it may have some unexpected consequences.

Hopefully solves the issues recently mentioned in #17.

lib/pytis/form/list.py
@@ -3048,8 +3048,10 @@ class SideBrowseForm(BrowseForm):
             return super(SideBrowseForm, self)._get_aggregation_result(key)
 
     def _provider_kwargs(self):
-        return dict(super(SideBrowseForm, self)._provider_kwargs(),
-                    main_form_row=self._main_form_row)
+        kwargs = super(SideBrowseForm, self)._provider_kwargs()
+        if kwargs:
+            kwargs['main_form_row'] = self._main_form_row
+        return kwargs
 
     def on_selection(self, row):
         """Update form after main form selection.

lib/pytis/form/form.py
@@ -994,8 +994,7 @@ class LookupForm(InnerForm):
         self._lf_provider_condition = None
         self._arguments = arguments
         self._query_field_values = query_field_values and dict(query_field_values) or None
-        if not self._apply_providers():
-            raise Form.InitError()
+        self._apply_providers(initial=True)
         self._lf_select_count_ = None
         self._init_select(async_count=True)
 
@@ -1073,32 +1072,37 @@ class LookupForm(InnerForm):
             kwargs['query_fields'] = self._query_fields_row()
         return kwargs
 
-    def _apply_providers(self):
-        condition_provider = self._view.condition_provider()
-        argument_provider = self._view.argument_provider()
-        if condition_provider or argument_provider:
-            kwargs = self._provider_kwargs()
-            if 'query_fields' in kwargs and kwargs['query_fields'] is None:
-                # If _query_fields_row() returs None, it means that the
+    def _apply_providers(self, initial=False):
+        kwargs = self._provider_kwargs()
+        if kwargs:
+            if kwargs['query_fields'] is None:
+                # If _query_fields_row() returns None, it means that the
                 # query fields are not initialized yet and autoinit is off.
                 # This happens during form initialization.  In this case we
                 # want to display an empty form without calling the provider
-                # functions at all.
-                if condition_provider:
-                    self._lf_provider_condition = pytis.data.OR()
-                if argument_provider:
-                    # UNKNOWN_ARGUMENTS result in an empty dummy select without
-                    # calling the underlying database function.
-                    self._arguments = self._data.UNKNOWN_ARGUMENTS
+                # functions at all.  We actually want to show empty form
+                # even if condition_provider/argument_provider is not
+                # defined.
+                # UNKNOWN_ARGUMENTS result in an empty dummy select without
+                # calling the underlying database function.
+                self._arguments = self._data.UNKNOWN_ARGUMENTS
+                # We use an empty OR() condition (always false) to make the
+                # select return no data.  It might seem better to have
+                # something like self._data.UNKNOWN_CONDITION, but it doesn't
+                # really make much difference in case of tables/views.
+                self._lf_provider_condition = pytis.data.OR()
             else:
+                argument_provider = self._view.argument_provider()
+                if argument_provider:
+                    self._arguments = argument_provider(**kwargs)
+                    if initial and self._arguments is None:
+                        raise Form.InitError()
+                condition_provider = self._view.condition_provider()
                 if condition_provider:
                     self._lf_provider_condition = condition_provider(**kwargs)
-                if argument_provider:
-                    arguments = argument_provider(**kwargs)
-                    if arguments is None:
-                        return False
-                    self._arguments = arguments
-        return True
+                else:
+                    # Make sure to unset pytis.data.OR() set above.
+                    self._lf_provider_condition = None
 
     def _load_profiles(self):
         return profile_manager().load_profiles(self._profile_spec_name(), self._form_name(),

commit ba63bc41c33d6c26c480826b930fde70f451f043

Always add main_form_row to provider_kwargs in SideBrowseForm
Commit 660f5066aa caused different problems for argument_providers.
One attempt to solved them was made in the commit eb35453296.
This commit however unmasked another problem in original commit
660f5066aa. If no query_fields are defined in SideBrowseForm
then no mask_form_row would be added to provider_kwargs.

This commit should fix the original bug in 660f5066aa.

lib/pytis/form/list.py
@@ -3049,8 +3049,7 @@ class SideBrowseForm(BrowseForm):
 
     def _provider_kwargs(self):
         kwargs = super(SideBrowseForm, self)._provider_kwargs()
-        if kwargs:
-            kwargs['main_form_row'] = self._main_form_row
+        kwargs['main_form_row'] = self._main_form_row
         return kwargs
 
     def on_selection(self, row):