937061524f59103a2f6dfb6a47e19aba3780514f
Tomáš Cerha <t.cerha@gmail.com>
2020-08-13 10:15:57 +0200

    Add missing cms.py into pytis.demo
    This is according to the documentation in pytis.cms.

---
+++ lib/pytis/demo/cms.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019, 2020 Tomáš Cerha <t.cerha@gmail.com>
+# Copyright (C) 2009-2015 OUI Technology Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pytis.cms import (  # noqa: F401
+    Menu, MenuParents, Languages, Modules, Actions, GenericActions,
+    Users, Roles, SystemRoles, AllRoles, UserRoles, RoleUsers, Rights,
+    SessionLog, AccessLog, UserSessionLog, Themes,
+)
