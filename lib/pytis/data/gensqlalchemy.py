26dba8b464bb3c65805ca0dc08a294a8bb165210
Pavel Hanák <hanak@is-it.eu>
2021-05-17 20:00:54 +0200

    SQLFunction.from_clause should not return select
    If we call the SQL function as a part of a from clause,
    we mostly prefer not to generate the subselect call.
    The previous behaviour is still possible to get
    by using the new method "from_clause_select".

--- lib/pytis/data/gensqlalchemy.py
+++ lib/pytis/data/gensqlalchemy.py
@@ -3419,12 +3419,18 @@ class SQLFunctional(_SQLReplaceable, _SQLTabular):
         return [c.sqlalchemy_column(None, None, None, None)
                 for c in class_.specification_fields()]
 
-    def from_clause(self, *args):
+    def from_clause_select(self, *args):
         "Calls function with args and returns select for use in from clause"
         return sqlalchemy.select(
             self.selectable_fields(),
             from_obj=[self.__call__(*args)]
         )
+
+    def from_clause(self, *args):
+        "Calls function with args for use in from clause"
+        if not self.db_name:
+            self.db_name = self.name
+        return getattr(sqlalchemy.sql.expression.func, self.db_name)(*args)
 
     def _add_dependencies(self):
         super(SQLFunctional, self)._add_dependencies()
