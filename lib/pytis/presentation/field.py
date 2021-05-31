32ebc2ee6f6d33599a9802f55b73add89d03736a
Petr Malivanek <malivanek@is-it.eu>
2021-05-31 17:39:45 +0200

    Dočasný hack řešící nemožnost zadávat UD v SPVC.

--- lib/pytis/presentation/field.py
+++ lib/pytis/presentation/field.py
@@ -852,6 +852,8 @@ class PresentedRow(object):
             return False
         if self.hidden_codebook(key):
             return False
+        if key == "tdoklad":
+            return False
         return bool(self._computed_value(self._coldict[key].editable))
 
     def visible(self, key):
