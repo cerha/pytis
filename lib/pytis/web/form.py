b76be37ed2af3452110aa96f7e4aa638fa65d0fa
Tomáš Cerha <t.cerha@gmail.com>
2022-03-31 10:56:08 +0200

    Revert "Rewrite JavaScript to ES6/jQuery"
    This reverts commit 65acab8d332b72b12d34a124ad9ad7e703b7e940.
    
    Acidentally pushed from unfinished branch...

--- lib/pytis/web/form.py
+++ lib/pytis/web/form.py
@@ -785,7 +785,8 @@ class EditForm(_SingleRecordForm, _SubmittableForm):
             help_id = field_id + '-help'
             # Set through a script to avoid invalid HTML ('aria-describedby' in not valid HTML).
             return (g.div(descr, id=help_id, cls="help") +
-                    g.script("$('#%s').attr('aria-describedby', '%s');" % (field_id, help_id)))
+                    g.script("$('%s').setAttribute('aria-describedby', '%s');" %
+                             (field_id, help_id)))
         else:
             return None
 
