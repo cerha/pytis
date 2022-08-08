a77614c5d8bfaa3fa82c2989461cac947df0022f
Tomáš Cerha <t.cerha@gmail.com>
2022-08-08 16:22:10 +0200

    Fix relative import in doc/examples
    Fixes Python 3 compatibility...

--- doc/examples/dbdefs/__init__.py
+++ doc/examples/dbdefs/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2019 Tomáš Cerha <t.cerha@gmail.com>
+# Copyright (C) 2019-2022 Tomáš Cerha <t.cerha@gmail.com>
 # Copyright (C) 2012 OUI Technology Ltd.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -16,4 +16,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from demo import *
+from .demo import *
