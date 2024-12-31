8b681e8b7b466c86ac103b4b4b19d7dd9ff72bf5
Tomáš Cerha <t.cerha@gmail.com>
2024-12-31 13:13:22 +0100

    Unify GPL headers in all files
    Use the version with FSF web link instead of the postal address.

--- lib/pytis/defs/__init__.py
+++ lib/pytis/defs/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2019 Tomáš Cerha <t.cerha@gmail.com>
+# Copyright (C) 2018-2024 Tomáš Cerha <t.cerha@gmail.com>
 # Copyright (C) 2001-2011 OUI Technology Ltd.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -14,8 +14,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """Pytis specification files.
 
