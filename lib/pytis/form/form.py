cerha/pytis

commit d5343e3601cbede6c3d613f0f5975a1096959bde

Implement CurrentRowIterator.__next__()
Otherwise actions fail when next(rows) is called when there is no active
selection (the current row is then used as the selection).

lib/pytis/form/form.py
@@ -2091,12 +2091,16 @@ class RecordForm(LookupForm):
 
                     def __init__(self, form):
                         self._form = form
+                        self._iter = iter([form.current_row()])
 
                     def __len__(self):
                         return 1
 
                     def __iter__(self):
-                        return iter([self._form.current_row()])
+                        return self
+
+                    def __next__(self):
+                        return next(self._iter)
 
                     @property
                     def form(self):

commit a5e40c4daf25405e9704c73e05cf2f9135bf558a

Fix rows iterator problems when no selection
The change introduced by commit d5343e360 didn't work correctly when the
context action method copied the iterator using copy.copy(rows).  This commit
removes the unnecessary additional class CurrentRowIterator and uses the same
iterator class in both cases to get consistent behavior when there is an active
selection and when there is no selection.

lib/pytis/form/form.py
@@ -2083,36 +2083,11 @@ class RecordForm(LookupForm):
             return None
 
     def _context_action_args(self, action):
-        def selection(form):
-            result = self.selected_rows()
-            if len(result) == 0:
-                class CurrentRowIterator(object):
-                    """See '_grid.TableRowIterator' for documentation."""
-
-                    def __init__(self, form):
-                        self._form = form
-                        self._iter = iter([form.current_row()])
-
-                    def __len__(self):
-                        return 1
-
-                    def __iter__(self):
-                        return self
-
-                    def __next__(self):
-                        return next(self._iter)
-
-                    @property
-                    def form(self):
-                        return self._form.provider()
-
-                result = CurrentRowIterator(form)
-            return result
         context = action.context()
         if context == ActionContext.RECORD:
             args = (self.current_row(),)
         elif context == ActionContext.SELECTION:
-            args = (selection(self),)
+            args = (self.selected_rows(fallback_to_current_row=True),)
         else:
             raise ProgramError("Unsupported action context:", context)
         scontext = action.secondary_context()
@@ -2478,8 +2453,12 @@ class RecordForm(LookupForm):
         """
         return self._row
 
-    def selected_rows(self):
+    def selected_rows(self, fallback_to_current_row=False):
         """Return an iterator over all currently selected rows.
+
+        Arguments:
+          fallback_to_current_row -- if True and if there is no selection,
+            return iterator over the current row.
 
         The iterator returns all rows present in the current selection as
         'PresentedRow' instances in the order of ther presence in the form.

lib/pytis/form/list.py
@@ -702,8 +702,11 @@ class ListForm(RecordForm, Refreshable):
                 pass
         return result
 
-    def selected_rows(self):
-        return TableRowIterator(self._table, self._grid.GetSelectedRows())
+    def selected_rows(self, fallback_to_current_row=False):
+        row_numbers = self._grid.GetSelectedRows()
+        if len(row_numbers) == 0 and fallback_to_current_row:
+            row_numbers = (self._table.current_row(),)
+        return TableRowIterator(self._table, row_numbers)
 
     def unselect_selected_rows(self):
         self._grid.ClearSelection()
@@ -2690,10 +2693,7 @@ class SelectRowsForm(CodebookForm):
     """Řádkový pop-up formulář vracející tuple všech vybraných řádků."""
 
     def _on_activation(self, alternate=False):
-        selected_rows = tuple(self.selected_rows())
-        if len(selected_rows) == 0:
-            selected_rows = (self.current_row(),)
-        self._result = selected_rows
+        self._result = tuple(self.selected_rows(fallback_to_current_row=True))
         self.Parent.EndModal(1)
         return True
 