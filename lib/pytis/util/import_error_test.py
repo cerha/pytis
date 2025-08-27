d3c6aac4da54a7a946cc4301614ec995b99773de
Tomáš Cerha <t.cerha@gmail.com>
2025-08-27 12:06:11 -0500

    Add resolver exceptions and module import tests

---
+++ lib/pytis/util/import_error_test.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018-2025 Tomáš Cerha <t.cerha@gmail.com>
+# Copyright (C) 2001-2018 OUI Technology Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Module with failing imports to test resolver error handling."""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import foo_bar_xy  # noqa: F401  (intentional failing import for tests)
