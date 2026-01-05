6b25910dc7a988b4b87a8eb24dc891914c14ccf8
Tomáš Cerha <t.cerha@gmail.com>
2026-01-05 17:30:43 +0100

    Turn translation source messages to English in pytis-defs

--- lib/pytis/defs/crypto.py
+++ lib/pytis/defs/crypto.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2019-2023, 2025 Tomáš Cerha <t.cerha@gmail.com>
+# Copyright (C) 2019-2026 Tomáš Cerha <t.cerha@gmail.com>
 # Copyright (C) 2014-2015 OUI Technology Ltd.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -27,20 +27,20 @@ from pytis.util import rsa_encrypt, translations
 from pytis.dbdefs import db_pytis_crypto
 from pytis.dbdefs.db_pytis_crypto import PytisCryptoDbKey
 
-_ = translations('pytis')
+_ = translations('pytis-defs')
 
 
 class NewAdminPasswd(Specification):
     public = True
 
     data_cls = pytis.data.RestrictedMemData
-    title = _(u"Změna administrátorského hesla")
+    title = _(u"Change administrator password")
 
     def fields(self):
         return (
-            Field('old_password', _(u'Stávající heslo'), width=20,
+            Field('old_password', _(u'Current password'), width=20,
                   type=pytis.data.Password, md5=False, verify=False),
-            Field('new_password', _(u'Nové heslo'), width=20,
+            Field('new_password', _(u'New password'), width=20,
                   type=pytis.data.Password, maxlen=32, minlen=8, md5=False,
                   strength=self._check_strength),
         )
@@ -55,18 +55,18 @@ class NewAdminPasswd(Specification):
             elif char.isdigit():
                 digits = True
         if not (lower and upper and digits):
-            return _(u"Použijte, prosím, kombinaci malých a velkých znaků a číslic.")
+            return _(u"Please use a combination of lowercase and uppercase letters and digits.")
 
 
 class CryptoAreas(Specification):
     public = True
     table = db_pytis_crypto.CPytisCryptoNames
-    title = _(u"Šifrovací oblasti")
+    title = _(u"Encryption areas")
     sorting = (('name', pytis.data.ASCENDENT,),)
-    bindings = (Binding('users', _(u"Uživatelé"), 'crypto.Users', binding_column='name'),)
+    bindings = (Binding('users', _(u"Users"), 'crypto.Users', binding_column='name'),)
 
     def actions(self):
-        return (pytis.presentation.Action('change_password', _(u"Změnit heslo administrátora"),
+        return (pytis.presentation.Action('change_password', _(u"Change administrator password"),
                                           self._change_admin_password),)
 
     def _change_admin_password(self, row):
@@ -77,14 +77,14 @@ class CryptoAreas(Specification):
         connection_data = pytis.config.dbconnection
         key_id, key = pytis.extensions.crypto_admin_key(area, 'admin', connection_data)
         if not key_id or not key:
-            app.error(_(u"Nebyl nalezen klíč administrátora pro tuto oblast"))
+            app.error(_(u"Administrator key for this area was not found"))
             return
         row = app.new_record("crypto.NewAdminPasswd", multi_insert=False)
         if not row:
             return
         old_password = row["old_password"].value()
         if not pytis.extensions.check_crypto_password(key, old_password, connection_data):
-            app.error(_(u"Chybné heslo"))
+            app.error(_(u"Incorrect password"))
         encrypted_old_password = rsa_encrypt(db_key, old_password)
         new_password = row["new_password"].value()
         encrypted_new_password = rsa_encrypt(db_key, new_password)
@@ -93,9 +93,9 @@ class CryptoAreas(Specification):
                               ('old_psw', pytis.data.sval(encrypted_old_password),),
                               ('new_psw', pytis.data.sval(encrypted_new_password),),))
         if not function.call(row)[0][0].value():
-            app.error(_(u"Heslo se změnit nepodařilo"))
+            app.error(_(u"Failed to change password"))
             return
-        app.message(_(u"Heslo bylo změněno"))
+        app.message(_(u"Password has been changed"))
 
     def on_new_record(self, *args, **kwargs):
         return None
@@ -110,7 +110,7 @@ class CryptoAreas(Specification):
 class Users(Specification):
     public = True
     table = db_pytis_crypto.EPytisCryptoKeys
-    title = _(u"Uživatelé")
+    title = _(u"Users")
 
     def _customize_fields(self, fields):
         fields.modify('name',
@@ -120,19 +120,19 @@ class Users(Specification):
                       editable=pytis.presentation.Computer(
                           self._editable, depends=('name',)))
         fields.append(
-            Field('admin_password', _(u"Heslo přihlášeného uživatele"),
+            Field('admin_password', _(u"Logged-in user's password"),
                   width=24, virtual=True,
                   type=pytis.data.Password(not_null=True, verify=False),
                   editable=pytis.presentation.Computer(self._editable, depends=('name',))
                   ))
         fields.append(
-            Field('admin_address', _(u"E-mailová adresa administrátora"),
+            Field('admin_address', _(u"Administrator e-mail address"),
                   width=24, virtual=True,
                   type=pytis.data.String(not_null=True),
                   editable=pytis.presentation.Computer(self._editable, depends=('name',)),
                   ))
         fields.append(
-            Field('user_password', _(u"Nové heslo pro uživatele"),
+            Field('user_password', _(u"New password for user"),
                   virtual=True, type=pytis.data.String(),
                   editable=pytis.presentation.Editable.NEVER
                   ))
@@ -146,9 +146,9 @@ class Users(Specification):
             pytis.presentation.HGroup(
                 'user_password',
                 pytis.presentation.Button(
-                    _(u"Generovat heslo"), self._gen_password),
+                    _(u"Generate password"), self._gen_password),
                 pytis.presentation.Button(
-                    _(u"Zrušit heslo"), self._clear_password)
+                    _(u"Clear password"), self._clear_password)
             ))
 
     def _editable(self, row, col):
@@ -187,7 +187,7 @@ class Users(Specification):
         for f in fields:
             if f.virtual():
                 fields.modify(f.id(), virtual=False)
-        record = app.input_form(title=_(u"Nový uživatel šifrované oblasti"),
+        record = app.input_form(title=_(u"New encrypted area user"),
                                 fields=fields, layout=self.view_spec().layout(),
                                 check=self.view_spec().check(), prefill={'name': area})
         if not record:
@@ -217,19 +217,19 @@ class Users(Specification):
         connection_data = pytis.config.dbconnection
         key_id, key = pytis.extensions.crypto_admin_key(area, pytis.config.dbuser, connection_data)
         if not key_id or not key:
-            app.error(_(u"Nebyl nalezen klíč pro tuto oblast"))
+            app.error(_(u"Key for this area was not found"))
             return 'name'
         crypto_password = row['admin_password'].value()
         if not pytis.extensions.check_crypto_password(key, crypto_password, connection_data):
-            app.error(_(u"Chybné heslo"))
+            app.error(_(u"Incorrect password"))
             return 'admin_password'
 
     def on_edit_record(self, row):
-        app.warning(_(u"Uživatele lze jen přidávat a odebírat"))
+        app.warning(_(u"Users can only be added or removed"))
         return None
 
     def on_delete_record(self, row):
         if row['username'].value() == 'admin':
-            app.error(_(u"Administrátory mazat nelze"))
+            app.error(_(u"Administrators cannot be deleted"))
             return None
         return True
