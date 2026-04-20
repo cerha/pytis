cc01736148e8bdeef7fe717a1f8e54461155fafd
Tomáš Cerha <cerha@truecode.cz>
2026-04-20 16:09:05 +0200

    Merge tools/test_handlers.py into pytis/remote/test.py
    pytis/remote/test.py already existed as a pytest suite for the same
    remote-session functionality.  tools/test_handlers.py duplicated most of the
    existing tests, but added its own runner, protocol adapters, and manual
    connect/push logic that largely reimplemented what pytis.remote already
    provides.  These layers are unnecessary as we only need to test the final
    functionality provided by the remote API.  Thus the adapters were dropped in
    favour of calling pytis.remote directly.
    
    The unique coverage from test_handlers.py was merged into pytis/remote/test.py:
    
    - add test_client_info, test_connection_echo, test_open_file,
      test_open_selected_file, test_make_selected_file
    - expand test_file_io (flush, readline, readlines, name, partial read)
      and test_run_python (additional exit codes)
    - interactive tests print instructions for the required user actions

--- pytis/remote/test.py
+++ pytis/remote/test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2018-2025 Tomáš Cerha <t.cerha@gmail.com>
+# Copyright (C) 2018-2026 Tomáš Cerha <t.cerha@gmail.com>
 # Copyright (C) 2011-2018 OUI Technology Ltd.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -16,6 +16,20 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Tests for `pytis.remote` in a live Pytis2Go session.
+
+Run inside an X2Go session with an active pytis2go connection:
+
+`pytis/remote/test.py`
+
+Interactive tests open file-chooser dialogs on the client machine; enable them
+by setting the environment variable `PYTIS_TEST_INTERACTIVE` or passing
+`--interactive` command line argument:
+
+`pytis/remote/test.py --interactive`
+
+"""
 
 from __future__ import print_function
 from __future__ import unicode_literals
@@ -33,14 +47,11 @@ def interactive(test):
 
 
 class TestRemote:
-    """Test functions in `pytis.remote` in remote mode.
+    """Tests for `pytis.remote` functions in a live Pytis2Go session.
 
-    This test must be run through Pytis2go.  Launch a terminal through Pytis2go
-    client and run the test inside the terminal.
-
-    This test is only run if the remote environment exists.  Particularly if the
-    environment variable X2GO_SESSION exists and the session file in ~/.x2go/ssh
-    contains connection data.
+    The test suite must run inside an active X2Go session connected to a
+    pytis2go client.  The entire class is skipped when `X2GO_SESSION` is not
+    set.
 
     """
 
@@ -48,25 +59,51 @@ class TestRemote:
     def setup_class(cls):
         if not os.getenv('X2GO_SESSION'):
             pytest.skip("Not within an X2Go session.")
-        # Avoid removing the info file (to allow running tests multiple times).
         pytis.remote.keep_x2go_info_file()
+        if not pytis.remote.client_connection_ok():
+            pytest.skip("pytis2go connection not available.")
 
-    def test_clientapi_loaded(script):
+    def test_clientapi_loaded(self):
         assert pytis.remote.RPCInfo.client_api_pushed
+
+    def test_client_info(self):
+        info = pytis.remote.client_info()
+        assert info is not None
+        assert info.os_name is not None
+        assert info.python_version is not None
+
+    def test_connection_echo(self):
+        assert pytis.remote.client_connection_ok()
 
     def test_clipboard(self):
         pytis.remote.set_clipboard_text('foo')
         assert pytis.remote.get_clipboard_text() == 'foo'
 
-    def test_basic_file_operations(self):
-        with pytis.remote.open_file('test.txt', mode='wb') as f:
-            f.write(b'abc')
-            f.flush()  # Just to test that the call does not fail...
-        with pytis.remote.open_file('test.txt', mode='rb') as f:
-            assert f.read() == b'abc'
+    def test_file_io(self):
+        test_data = b'line one\nline two\nline three\n'
+        with pytis.remote.make_temporary_file(suffix='.bin', mode='w+b') as f:
+            f.write(test_data)
+            f.flush()
             f.seek(0)
-            assert f.read(1) == b'a'
-            assert f.read(10) == b'bc'
+            assert f.read() == test_data
+            f.seek(0)
+            assert f.readline() == b'line one\n'
+            f.seek(0)
+            assert list(f.readlines()) == [b'line one\n', b'line two\n', b'line three\n']
+            assert f.name is not None
+
+    def test_open_file(self):
+        content = b'open_file test \xc4\x8d\xc5\x99'
+        with pytis.remote.make_temporary_file(suffix='.bin', mode='wb') as f:
+            f.write(content)
+            fname = f.name
+        with pytis.remote.open_file(fname, mode='rb') as f:
+            assert f.read(4) == b'open'
+            assert f.read() == b'_file test \xc4\x8d\xc5\x99'
+        with pytis.remote.open_file(fname, mode='wb') as f:
+            f.write(b'replaced')
+        with pytis.remote.open_file(fname, mode='rb') as f:
+            assert f.read() == b'replaced'
 
     def test_encoded_file_operations(self):
         with pytis.remote.make_temporary_file(suffix='.txt', mode='w', encoding='utf-8') as f:
@@ -86,7 +123,7 @@ class TestRemote:
             assert tuple(f.readlines()) == (b'\xc5\xbdlu\xc5\xa5ou\xc4\x8dk\xc3\xbd\n',
                                             b'k\xc5\xaf\xc5\x88\n')
 
-    def test_run_python(script):
+    def test_run_python(self):
         with pytis.remote.make_temporary_file(suffix='.txt', mode='wb') as f:
             f.write(b'a')
             fname = f.name
@@ -97,14 +134,46 @@ class TestRemote:
         assert pytis.remote.run_python('print("hello world")') == 0
         assert pytis.remote.run_python('script_with_some_error()') == 1
         assert pytis.remote.run_python('import sys; sys.exit(24)') == 24
+        assert pytis.remote.run_python('import sys; sys.exit(42)') == 42
 
     @interactive
     def test_file_dialogs(self):
-        directory = pytis.remote.select_directory(title=("Select a directory"))
+        print("\n    -> SELECT any directory...")
+        directory = pytis.remote.select_directory(title="Select a directory")
         assert directory is not None
+        print("       selected: {}".format(directory))
+        print("    -> SELECT a PDF file...")
         path = pytis.remote.select_file(directory=directory, pattern='*.pdf',
                                         title="Select a PDF file in this directory")
-        assert path and path.startswith(directory)
-        with pytis.remote.open_file(path, mode='r') as f:
+        assert path
+        with pytis.remote.open_file(path, mode='rb') as f:
             assert f.read(4) == b'%PDF'
+        print("       selected: {}".format(path))
+        print("    -> CHECK that the file opens on the client...")
         pytis.remote.launch_file(path)
+
+    @interactive
+    def test_open_selected_file(self):
+        print("\n    -> SELECT any file in the open dialog that appears on the client...")
+        result = pytis.remote.open_selected_file(patterns=[], pattern=None)
+        assert result is not None
+        data = result.read(64)
+        result.close()
+        print("       first {} bytes read OK".format(len(data)))
+
+    @interactive
+    def test_make_selected_file(self):
+        print("\n    -> SAVE to any location in the save dialog that appears on the client...")
+        result = pytis.remote.make_selected_file(mode='wb', patterns=[], pattern=None)
+        assert result is not None
+        result.write(b'pytis2go test write')
+        result.close()
+        print("       saved OK")
+
+
+if __name__ == '__main__':
+    import sys
+    args = [__file__, '-v', '-s']
+    if '--interactive' in sys.argv:
+        os.environ['PYTIS_TEST_INTERACTIVE'] = '1'
+    sys.exit(pytest.main(args))
